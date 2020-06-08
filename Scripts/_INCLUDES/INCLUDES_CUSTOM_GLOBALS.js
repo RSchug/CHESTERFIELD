54066ba128da52e04110f22c91413962ca152c30
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-08 15:09:34 -0700

    SUPP

--- Scripts/_INCLUDES/INCLUDES_CUSTOM_GLOBALS.js
+++ Scripts/_INCLUDES/INCLUDES_CUSTOM_GLOBALS.js
@@ -29,11 +29,13 @@ if (matches(publicUserID, "PUBLICUSER1182", "PUBLICUSER1202")) {
 
 showDebug = false;
 if (currentUserID == "ADMIN") showDebug = true;
+if (currentUserID == "HOBDAYK") showDebug = true;
 if (currentUserID == "KHOBDAY") showDebug = 3;
 if (currentUserID == "DBOUCHER") showDebug = true;
 if (currentUserID == "MBOUQUIN") showDebug = true;
 if (currentUserID == "JGUEST") showDebug = true;
 if (currentUserID == "TRUEPOINT") showDebug = 3;
+if (currentUserID == "RSCHUG") showDebug = true;
 
 /*------------------------------------------------------------------------------------------------------/
 | END Custom Parameters
