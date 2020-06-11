afb43c2f426adf2558280f293d6596fcfb654950
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-11 13:12:48 -0700

    14B Include Temporary CO Issued

--- Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
@@ -1,7 +1,7 @@
 // ISB:Building/Permit/*/*
 // ISB:BUILDING/PERMIT/*/*
 // Permit must be Issued
-if (!wasCapStatus(["Issued"])) {
+if (!wasCapStatus(["Issued","Temporary CO Issued"])) {
         showMessage = true;
         comment('<font size=small><b>Record must be Issued to schedule inspections</b></font>');
         cancel = true;
