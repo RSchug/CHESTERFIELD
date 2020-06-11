8396daccc3d2812b609c79591d5abbeaacdfc8b5
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-11 13:53:29 -0700

    Fix Deleting Structure

--- Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!DEMOLITION.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!DEMOLITION.js
@@ -3,11 +3,15 @@ if (wfTask == "Inspections" && wfStatus == "Completed"){
     if (parentCapId && appMatch("Building/Structure/NA/NA", parentCapId)) {
         logDebug("Updating Structure " + parentCapId.getCustomID() + " to Demolished");        updateAppStatus("Demolished", "Updated via script from " + capId.getCustomID(), parentCapId);
         logDebug("Deleting Structure " + parentCapId.getCustomID());
-        var removeRecordResult = aa.cap.removeRecord(parentCapId);
-        if (removeRecordResult.getSuccess()) {
-            logDebug("Successfully deleted structure " + parentCapId.getCustomID());
-        } else {
-            logDebug("Failed to deleted structure " + parentCapId.getCustomID() + ". Reason: " + removeRecordResult.getErrorMessage());
+        try {
+            var removeRecordResult = aa.cap.removeRecord(parentCapId);
+            if (removeRecordResult.getSuccess()) {
+                logDebug("Successfully deleted structure " + parentCapId.getCustomID());
+            } else {
+                logDebug("Failed to deleted structure " + parentCapId.getCustomID() + ". Reason: " + removeRecordResult.getErrorMessage());
+            }
+        } catch (err) {
+                logDebug("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: " + err.stack);
         }
     }
 }
