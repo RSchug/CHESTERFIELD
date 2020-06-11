841ff6bd4fe4c6c5db1d6302cf574a9413f6039e
bushatos <66446858+bushatos@users.noreply.github.com>
2020-06-11 13:35:40 -0400

    de test

--- Scripts/Event/AIUA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/AIUA;BUILDING!PERMIT!~!~.js
@@ -2,6 +2,7 @@
 editAppSpecific("Number of Units", houseCount);
 
 
+
 //if recordType ("Building/Permit/Residential/Boiler")
 //addFee("BOILER","CC-BLD-RES-BOILER","FINAL",houseCount,"Y");
 //addFee("STATELEVY","CC-BLD-RES-BOILER","FINAL",1,"Y");
