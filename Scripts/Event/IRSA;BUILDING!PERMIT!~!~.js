cabe538fe1da8f5707b2bb978a1e9067f8dff276
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-11 09:11:23 -0700

    35B Existing Structure

--- Scripts/Event/IRSA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/IRSA;BUILDING!PERMIT!~!~.js
@@ -13,3 +13,23 @@ if (inspType.equals("Amusement Final") && inspResult.equals("Approved")){
 	logDebug("Updating " + expField + " to " + expDateNew);
 	editAppSpecific(expField, expDateNew);
 }
+// 35B: For Record Types: Residential Bldg, Residential Multi-Family and Commercial Building
+// If the Inspection Result is 'Approved' on Inspection Type of 'Building Final', and the related 
+// BUILDING/STRUCTURE Record Status is 'New Building' update Record Status on BUILDING/STRUCTURE to 'Existing'
+if (inspType.equals("Building Final") && inspResult.equals("Approved")
+&& (appMatch("Building/Permit/Residential/NA") || appMatch("Building/Permit/Residential/Multi-Family") || appMatch("Building/Permit/Commercial/NA"))) {
+	if (parentCapId && appMatch("Building/Structure/NA/NA", parentCapId)) {
+		var parentCapStatus = null;
+		var getCapResult = aa.cap.getCap(parentCapId);
+		if (getCapResult.getSuccess()) {
+			var parentCap = getCapResult.getOutput();
+			parentCapStatus = parentCap.getCapStatus();
+		}
+		if (parentCapStatus == "New Building") {
+			logDebug("Updating Structure " + parentCapId.getCustomID() + " from " + parentCapStatus);
+			updateAppStatus("Existing", "Updated via script from " + capId.getCustomID(), parentCapId);
+		} else {
+			logDebug("Keeping Structure " + parentCapId.getCustomID() + " as " + parentCapStatus);
+		}
+	}
+}
