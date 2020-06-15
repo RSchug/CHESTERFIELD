3ac8e95bac5eb139465ea14caeee6ae092a9ce89
SYSTEM <SYSTEM@NONPROD-AA-0.us.accela.com>
2020-06-15 16:18:20 -0500

    commit by emse tools 2020-06-15 16:18:20

--- Scripts/Event/IRSA;ENFORCEMENT!ZONING CODE COMPLIANCE!NA!NA.js
+++ Scripts/Event/IRSA;ENFORCEMENT!ZONING CODE COMPLIANCE!NA!NA.js
@@ -1,12 +1,44 @@
-if (inspType == "Initial" && inspResult == "Violation") {var inspComment = inspObj.getInspection().getResultComment(); inspComment != null;
-                scheduleInspectDate("Follow-up",dateAdd(null,1,"Y"),currentUserID,null,inspComment);
-				closeTask("Investigation","In Violation","Updated Via Script",null);
-				activateTask("Notice"); updateTask("Notice","Pending Notice","Updated Via Script",null);
-				}
+//Enforcement/Zoning Code Compliance/*/* if Initial Inspection is In Violation then close Investigation Workflow with In Violation status and update Notice Workflow with Pending Notice status and schedule Follow-up Inspection for next day 
+if((matches(inspResult,"In Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","In Violation","Updated based on In Violation Initial Inspection","");
+	updateTask("Notice","Pending Notice","Updated based on In Violation Initial Inspection","");
+	scheduleInspection("Follow-up",1,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"10 Day In Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","In Violation","Updated based on 10 Day In Violation Initial Inspection","");
+	updateTask("Notice","Pending Notice","Updated based on In Violation Initial Inspection","");
+	scheduleInspection("Follow-up",13,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"14 Day In Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","In Violation","Updated based on 14 Day In Violation Initial Inspection","");
+	updateTask("Notice","Pending Notice","Updated based on In Violation Initial Inspection","");
+	scheduleInspection("Follow-up",17,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"30 Day In Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","In Violation","Updated based on 30 Day In Violation Initial Inspection","");
+	updateTask("Notice","Pending Notice","Updated based on In Violation Initial Inspection","");
+	scheduleInspection("Follow-up",33,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"60 Day In Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","In Violation","Updated based on 60 Day In Violation Initial Inspection","");
+	updateTask("Notice","Pending Notice","Updated based on In Violation Initial Inspection","");
+	scheduleInspection("Follow-up",63,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"No Violation") && inspType.equals("Initial")) && isTaskActive("Investigation")){
+	closeTask("Investigation","No Violation","Updated based on No Violation Initial Inspection","");
+	}
+if(matches(inspResult,"Administrative Review") && inspType.equals("Initial")){
+	scheduleInspection("Initial",14,currentUserID,null,"Auto Scheduled");
+	}
+if(matches(inspResult,"Administrative Review") && inspType.equals("Follow-up")){
+	scheduleInspection("Follow-up",14,currentUserID,null,"Auto Scheduled");
+	}
+if((matches(inspResult,"Abated - County") && inspType.equals("Follow-up")) && isTaskActive("Enforcement")){
+	closeTask("Enforcement","Administrative Abatement","Updated based on Abated - County Follow-up Inspection","");
+	updateTask("Final Processing","Pending Invoicing","Updated based on Abated - County Follow-up Inspection","");
+	} 
+if((matches(inspResult,"Abated - Owner") && inspType.equals("Follow-up")) && isTaskActive("Enforcement")){
+	closeTask("Enforcement","Complied","Updated based on Abated - Owner Follow-up Inspection","");
+	} 
 
 
-if (inspType == "Initial" && inspResult == "10 Day In Violation"){var inspComment = inspObj.getInspection().getResultComment(); inspComment != null;
-                scheduleInspectDate("Follow-up",dateAdd(null,13,"Y"),currentUserID,null,inspComment);
-				closeTask("Investigation","In Violation","Updated Via Script",null);
-				activateTask("Notice"); updateTask("Notice","Pending Notice","Updated Via Script",null); updateAppStatus("In Violation","")
-				}
