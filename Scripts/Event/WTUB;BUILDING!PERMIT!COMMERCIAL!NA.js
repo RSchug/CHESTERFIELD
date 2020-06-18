696cafc3e9156b03bb342709d77ab3faf7af8912
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-18 14:24:45 -0700

    46B Require Structure

--- Scripts/Event/WTUB;BUILDING!PERMIT!COMMERCIAL!NA.js
+++ Scripts/Event/WTUB;BUILDING!PERMIT!COMMERCIAL!NA.js
@@ -1,15 +1,19 @@
-//Nature of Work must be populated before Application Submittal Workflow Status is Accepted, Accepted - Plan Review Not Required, or Accepted - Plan Review Required//
-try {
-    if ((wfTask == 'Application Submittal' && (wfStatus == 'Accepted' || wfStatus == 'Accepted - Plan Review Not Required' || wfStatus == 'Accepted - Plan Review Required')) && (AInfo["Nature of Work"] == null)) 
-{
-        showMessage = true;
-        comment('<font size=small><b>Nature of Work Data Field is Required');
-        cancel = true;
+if (wfTask == 'Application Submittal' && exists(wfStatus,['Accepted','Accepted - Plan Review Not Required','Accepted - Plan Review Required'])) {
+	//Nature of Work must be populated before Application Submittal Workflow Status is Accepted, Accepted - Plan Review Not Required, or Accepted - Plan Review Required
+	if (typeof (AInfo["Nature of Work"]) == "undefined") {
+		// Nature of Work is not on this record type
+	} else if (AInfo["Nature of Work"] == null) {
+		showMessage = true;
+		comment('<font size=small><b>Nature of Work Data Field is Required</b></font>');
+		cancel = true;
+		// 46B. Structure must be linked except for Signs, and Commercial Towers, other accessory structures before Application Submittal Workflow Status is Accepted, Accepted - Plan Review Not Required, or Accepted - Plan Review Required
+	} else if (!exists(AInfo["Nature of Work"], ["Accessory Structure", "Communication Tower (includes related equipment shelters)"]) && !(parentCapId && appMatch("Building/Structure/NA/NA", parentCapId))) {
+		showMessage = true;
+		comment('<font size=small><b>Structure is Required</b></font>');
+		cancel = true;
+	}
 }
-    } catch (err) 
-{
-    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
-}
+
 /*
 Title : Final Plat Validation
 Purpose : The Final Plat record has data that should be validated as building permits are created.
