0904521ba11a5efd6b76c34b411e5731bb21905d
SYSTEM <SYSTEM@NONPROD-AA-0.us.accela.com>
2020-06-18 11:29:37 -0500

    commit by emse tools 2020-06-18 11:29:37

--- Scripts/Event/WTUB;BUILDING!MISCELLANEOUS!~!~.js
+++ Scripts/Event/WTUB;BUILDING!MISCELLANEOUS!~!~.js
@@ -10,3 +10,21 @@ try {
 {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
+//Address/Parcel/Owner must be entered before Initiation Workflow Status is Accepted//
+if ((wfTask == 'Initiation' && wfStatus == 'Accepted')) {
+	//Before Workflow Task Status can be selected - confirm that at least one Address, one Parcel and one Owner exists on Record.
+	if (!addressExistsOnCap()) {          // Check if address exists
+	   showMessage = true;
+	   comment('<font size=small><b>Address is required prior to Acceptance</b></font>');
+	   cancel = true;
+	}
+	if (!parcelExistsOnCap()) {             // Check if address exists
+	   showMessage = true;
+	   comment('<font size=small><b>Parcel is required prior to Acceptance</b></font>');
+	   cancel = true;
+	}
+	if (!ownerExistsOnCap()) {            // Check if address exists
+	   showMessage = true;
+	   comment('<font size=small><b>Owner is required prior to Acceptance</b></font>');
+	   cancel = true;
+}}
