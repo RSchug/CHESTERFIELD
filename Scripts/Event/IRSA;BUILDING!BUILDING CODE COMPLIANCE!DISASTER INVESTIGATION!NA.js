dd6b804be7271504df695957af67cff26d528efc
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-06-22 18:26:04 -0400

    NEW

---
+++ Scripts/Event/IRSA;BUILDING!BUILDING CODE COMPLIANCE!DISASTER INVESTIGATION!NA.js
@@ -0,0 +1,7 @@
+//IRSA:BUILDING/BUILDING CODE COMPLIANCE/DISASTER INVESTIGATION/NA
+if (inspResult.equals("No Damage")){
+	closeTask("Investigation","No Damage","Updated based on Inspection Result","");
+	}
+if (inspResult.equals("Damage")){
+	updateTask("Investigation","Damaged","Updated based on Inspection Result","");
+	}
