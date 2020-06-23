562190113c2b9dc45f393217020946fa142586cd
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-23 11:47:57 -0700

    Planning Fees

---
+++ Scripts/Event/ASA;PLANNING!~!~!~.js
@@ -0,0 +1,20 @@
+//Add Planning/LandUse/ManufacturedHomes/NA Fee
+if (appMatch("Planning/LandUse/ManufacturedHomes/NA")){
+addFee("MANUFACTURED","CC-PLANNING","FINAL",1,"Y");
+}
+//Add Planning/LandUse/AdminVariance/NA Fee
+if (appMatch("Planning/LandUse/AdminVariance/NA")){
+addFee("VARIANCEADM","CC-PLANNING","FINAL",1,"Y");
+}
+//Add Planning/LandUse/Variance/NA Fee
+if (appMatch("Planning/LandUse/Variance/NA")){
+addFee("VARIANCEBZA","CC-PLANNING","FINAL",1,"Y");
+}
+//Add Planning/LandUse/Appeal/NA Fee
+if (appMatch("Planning/LandUse/Appeal/NA")){
+addFee("APPEAL","CC-PLANNING","FINAL",1,"Y");
+}
+//Add Planning/LandUse/WrittenDetermination/NA Fee
+if (appMatch("Planning/LandUse/WrittenDetermination/NA")){
+addFee("WRITTEN","CC-PLANNING","FINAL",1,"Y");
+}
