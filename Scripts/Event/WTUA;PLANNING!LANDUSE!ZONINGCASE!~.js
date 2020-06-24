4acf79727abe04785d860511fbabaf58d78b551f
bushatos <66446858+bushatos@users.noreply.github.com>
2020-06-24 13:32:06 -0400

    testing

--- Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
@@ -1,52 +1,17 @@
 try {
     // Create Conditions from proffers on Zoning Case Record
 	if (wfTask == 'BOS Hearing' && matches(wfStatus, 'Approved')) {
-	    logDebug('I am in Zoning Case WTUA');
-		var profferTableArray = ['PROFFER CONDITIONS'];
-		r = aa.appSpecificTableScript.getAppSpecificTableGroupModel(profferTableArray).getOutput(),
-		s = r.getTablesArray(),
-		n = s.iterator(),
-		i = (new Array, new Array),
-		o = !1;
-		for (arguments.length > 2 && (i = arguments[2], o = !0); n.hasNext(); ) {
-			var g = n.next(),
-			u = new Array,
-			c = new Array,
-			l = g.getTableName() + "",
-			p = 0;
-			if (o) {
-				for (var d = !1, f = 0; f < i.length; f++)
-					if (i[f] == l) {
-						d = !0;
-						break
-					}
-				if (d)
-					continue
-			}
-			if (!g.rowIndex.isEmpty()) {
-				for (var m = g.getTableField().iterator(), C = g.getColumns().iterator(), b = g.getAppSpecificTableModel().getReadonlyField().iterator(), p = 1; m.hasNext(); ) {
-					if (!C.hasNext()) {
-						var C = g.getColumns().iterator();
-						c.push(u);
-						var u = new Array;
-						p++
-					}
-					var D = C.next(),
-					A = m.next(),
-					v = "N";
-					b.hasNext() && (v = b.next());
-					var y = new asiTableValObj(D.getColumnName(), A, v);
-					u[D.getColumnName()] = y
-				}
-				c.push(u)
-			}
-			logDebug("ASI Table Array : " + l + " (" + p + " Rows)");
-		}
-
+		var profferTableArray = loadASITable('PROFFER CONDITIONS');
+		logDebug('value of column a is : ' + columnA);
+		logDebug('value of column b is : ' + columnB);
+		logDebug('value of column c is : ' + columnC);
+		logDebug('value of column d is : ' + columnD);
+		logDebug('value of column e is : ' + columnE);
+		logDebug('value of column f is : ' + columnF);
 	}
 
 //            (AInfo['Temp Underground Electric'] == 'CHECKED')
-//            createPendingInspection('ELEC_COM', '208 Electric TUG Service');
+
 
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
