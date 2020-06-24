cbf62c9b0a9cce1811dc825dd38aa7fa272c15de
bushatos <66446858+bushatos@users.noreply.github.com>
2020-06-24 10:08:26 -0400

    testing

--- Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
@@ -1,8 +1,7 @@
 try {
     // Create Conditions from proffers on Zoning Case Record
-    if (wfTask == 'BOS Hearing' && matches(wfStatus, 'Approved') && balanceDue = 0) {
-		    logDebug('I am in Zoning Case WTUA');
-	}
+ //   if (wfTask == 'BOS Hearing' && matches(wfStatus, 'Approved') && balanceDue = 0) {
+//		    logDebug('I am in Zoning Case WTUA'); }
 
 //            (AInfo['Temp Underground Electric'] == 'CHECKED')
 //            createPendingInspection('ELEC_COM', '208 Electric TUG Service');
