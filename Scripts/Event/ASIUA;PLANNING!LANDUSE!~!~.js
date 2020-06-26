04be5561aa237f126d213cd7edb2a94f80fda957
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-26 16:04:25 -0700

    57P Sum Parcel Acreage

--- Scripts/Event/ASIUA;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/ASIUA;PLANNING!LANDUSE!~!~.js
@@ -6,7 +6,8 @@ Functional Area : ASIUA
 Description : Script will Sum values of column "Revised Acreage" in all rows in table CC-LU-TPA "Total Parcel Acreage"
 and set the sum into ASI "CC-LU-TPA-SUM.Total application acreage" 
  */
-
+// Moved to ASIUA:Planning/*/*/*
+/*
 useAppSpecificGroupName = true;
 var sum = 0;
 
@@ -21,3 +22,4 @@ if (tempAsit) {
 
 	editAppSpecific("CC-LU-TPA-SUM.Total application acreage", sum);
 }//asiTable
+*/
---
+++ Scripts/Event/ASIUA;PLANNING!~!~!~.js
@@ -0,0 +1,27 @@
+// ASIUA:Planning/*/*/*
+
+// 57P Summarize each row in the table data: CC - LU - TPA:
+// 'Parcel Acreage' to the Custom Field: 'Total Parcel Acreage'
+// 'Revised Acreage' to the Custom Field: 'Total application acreage'
+// Count the number of rows to the Custom Field: Total Number of parcels'
+// Copied from ASIUA:Planning/LANDUSE/*/*
+
+var tempAsit = loadASITable("CC-LU-TPA");
+if (tempAsit) {
+    var parcelAcreage = 0;
+    var revisedAcreage = 0;
+    var countParcels = 0;
+	for (a in tempAsit) {
+        if (!isNaN(tempAsit[a]["Parcel acreage"])) {
+            parcelAcreage += parseInt(tempAsit[a]["Parcel acreage"]);
+        }
+        if (!isNaN(tempAsit[a]["Revised acreage"])) {
+            revisedAcreage += parseInt(tempAsit[a]["Revised acreage"]);
+        }
+        countpParcels++;
+	}//for all rows
+	//editAppSpecific("CC-LU-TPA-SUM.Total application acreage", sum);
+    editAppSpecific("Total Parcel Acreage", parcelAcreage);
+    editAppSpecific("Total application acreage", revisedAcreage);
+    editAppSpecific("Total Number of parcels", countParcels);
+}
