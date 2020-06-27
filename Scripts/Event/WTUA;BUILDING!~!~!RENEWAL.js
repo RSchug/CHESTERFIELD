a06c76fd2829ca701aeeae7351dc181a9599295f
Ray Schug <RSchug@TruePointSolutions.com>
2020-06-27 06:24:48 -0700

    7B Elevator Renewed

--- Scripts/Event/WTUA;BUILDING!~!~!RENEWAL.js
+++ Scripts/Event/WTUA;BUILDING!~!~!RENEWAL.js
@@ -1,66 +1,66 @@
-/*
-
-WTUA;BUILDING!~!~!RENEWAL.js
-
- */
+// WTUA;BUILDING!~!~!RENEWAL.js
 
 //Alex Charlton added for Renewal of Conveyance Permit.
-if (wfStatus == 'Renewed') 
-   {
+if (wfStatus == 'Renewed') {
 	saveId = capId;
-	parentLicenseCAPID = getParentCapIDForReview(capId);
-	comment('ParentLic CAPID = ' + parentLicenseCAPID);
-	capId = parentLicenseCAPID;
-	if (parentLicenseCAPID) {
-		pCapIdSplit = String(parentLicenseCAPID).split('-');
-		pCapIdObj = aa.cap.getCapID(pCapIdSplit[0], pCapIdSplit[1], pCapIdSplit[2]).getOutput();
-		pCapIdCustomId = pCapIdObj.getCustomID();
-	}
-	if (parentLicenseCAPID) {
-		comment(' Parent Custom ID = ' + pCapIdCustomId);
-	}
+	var parentLicenseCapID = getParentCapIDForReview(capId);
+	logDebug('ParentLic CAPID = ' + parentLicenseCapID);
+	if (parentLicenseCapID) {
+		var pCapIdSplit = String(parentLicenseCapID).split('-');
+		var pCapIdObj = aa.cap.getCapID(pCapIdSplit[0], pCapIdSplit[1], pCapIdSplit[2]).getOutput();
+		var parentLicenseCustomID = pCapIdObj.getCustomID();
+		logDebug('ParentLic CustomID: ' + parentLicenseCustomID);
 
-	updateAppStatus('Active', 'Renewal Approved By: ' + capIDString, parentLicenseCAPID);
-
+		saveId = capId;
+		capId = parentLicenseCapID;
+		var wfCommentParent = "Renewal Approved By: " + capIDString;
+		//7B: For Elevator Renewal Record when Workflow Task 'Renewal Issuance' is 'Renewed', then update parent Elevator Permit Record Workflow Task 'Annual Status' to 'In Service' and Record Status to 'Active'
+		if (appMatch("Building/Permit/Elevator/Renewal")) {
+			resultWorkflowTask("Annual Status", "In Service", wfCommentParent, "");
+		}
+		updateAppStatus('Active', wfCommentParent, parentLicenseCapID);
 
         // Figure out new EXPIRATION Date
-
-		
-	lic = new licenseObject(null, capId);comment("Get License Object");
-	lic.setStatus('Active'); comment("Set Lic Exp Status to Active");
-
-        b1ExpResult = aa.expiration.getLicensesByCapID(parentLicenseCAPID);
-        this.b1Exp = b1ExpResult.getOutput();
-        tmpDate = this.b1Exp.getExpDate();
-        if (tmpDate){
-             this.b1ExpDate = tmpDate.getMonth() + "/" + tmpDate.getDayOfMonth() + "/" + tmpDate.getYear();
-             comment("In Building/*/Conveyance/*. This Lic Expires on " + this.b1ExpDate);
-
-	     //default to 12 months from today
-             numberOfMonths = 12; // 0 months because the EXP Code bumps it up 12 months
-
-          
-             // if Semi, change to 6 months form today        
-             if (AInfo['License Duration'] == 'Semi-Annual') {
-	         numberOfMonths = -6;  // -6 because the EXP Code bumps it up 12 months
-                 }
-
-             newExpDate = dateAddMonths(this.b1ExpDate,numberOfMonths);
-	     comment("newExpDate = "+newExpDate);
-
-             lic.setExpiration(newExpDate);
-
-             }
-
-
-
-	capId = saveId;
+		lic = new licenseObject(null, capId);
+		lic.setStatus('Active'); logDebug("Set Lic Exp Status to Active");
+		parentb1ExpResult = aa.expiration.getLicensesByCapID(parentLicenseCapID);
+		parentb1Exp = parentb1ExpResult.getOutput();
+		tmpDate = parentb1Exp.getExpDate();
+		if (tmpDate) {
+			tmpExpDate = tmpDate.getMonth() + "/" + tmpDate.getDayOfMonth() + "/" + tmpDate.getYear();
+			logDebug("Lic Expires on " + tmpExpDate);
+			//default to 12 months from date
+			numberOfMonths = 12; // 0 months because the EXP Code bumps it up 12 months
+			//7B: For Elevator Renewal Record when Workflow Task 'Renewal Issuance' is 'Renewed', then update the Permit Expiration Date, based on Quarter on the Elevator Permit Record (Building/Permit/Elevator/Annual).
+			if (appMatch("Building/Permit/Elevator/Renewal")) {
+				// Override if Annual Quarter set
+				var annualQuarter = getAppSpecific("Annual Quarter");
+				if (annualQuarter == "Q1 - March") {
+					tmpExpDate = "03/31/" + tmpDate.getYear();
+				} else if (annualQuarter == "Q2 - June") {
+					tmpExpDate = "06/30/" + tmpDate.getYear();
+				} else if (annualQuarter == "Q3 - September") {
+					tmpExpDate = "09/30/" + tmpDate.getYear();
+				} else if (annualQuarter == "Q4 - December") {
+					tmpExpDate = "12/31/" + tmpDate.getYear();
+				}
+				comment("tmpExpDate: " + tmpExpDate);
+			} else if (AInfo['License Duration'] == 'Semi-Annual') {
+	        	// if Semi, change to 6 months form today
+				numberOfMonths = -6;  // -6 because the EXP Code bumps it up 12 months
+			}
+			newExpDate = dateAddMonths(tmpExpDate, numberOfMonths);
+			// Check if less than today if so add another period.
+			if ((new Date(newExpDate)).getTime() < (new Date(aa.util.now())).getTime())
+				newExpDate = dateAddMonths(newExpDate, numberOfMonths);
+			comment("newExpDate: " + newExpDate);
+			lic.setExpiration(newExpDate);
+		}
+		capId = saveId;
 
         logDebug('Running WTUA4Renewal');
-	aa.runScript('WORKFLOWTASKUPDATEAFTER4RENEW');
-	logDebug('Messages in WTUA4Renewal:<br>' + aa.env.getValue('ScriptReturnMessage'));
-
-
-   } 
-
+		aa.runScript('WORKFLOWTASKUPDATEAFTER4RENEW');
+		logDebug('Messages in WTUA4Renewal:<br>' + aa.env.getValue('ScriptReturnMessage'));
+	} 
+}
 //commented out as this is included in lines above aa.runScript("WORKFLOWTASKUPDATEAFTER4RENEW");
