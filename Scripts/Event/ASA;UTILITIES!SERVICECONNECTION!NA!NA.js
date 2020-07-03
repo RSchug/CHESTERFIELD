303ee3ca9e9c4002aad50c7d47a7d1d571979026
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-03 12:30:00 -0400

    update

--- Scripts/Event/ASA;UTILITIES!SERVICECONNECTION!NA!NA.js
+++ Scripts/Event/ASA;UTILITIES!SERVICECONNECTION!NA!NA.js
@@ -1,2 +1,2 @@
-if (AInfo["Utility Type"] == "Water" && AInfo["Actual Meter Size"] == '5/8"' && !feeExists("WATERMETER")){
+if (AInfo["Utility Type"] == "Water" && AInfo["Actual Meter Size"] == "5/8" && !feeExists("WATERMETER")){
 	addFee("WATERMETER","CC-UTL-SC","FINAL",1,"Y")}
