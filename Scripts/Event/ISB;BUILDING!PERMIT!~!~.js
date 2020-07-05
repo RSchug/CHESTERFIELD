fc940d9c4d0e203838d0ce95e08d8d20ad70f237
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-05 17:10:40 -0400

    Added != Site Visit

--- Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
@@ -1,7 +1,6 @@
-// ISB:Building/Permit/*/*
 // ISB:BUILDING/PERMIT/*/*
-// Permit must be Issued
-if (!wasCapStatus(["Issued","Temporary CO Issued"])) {
+// Permit must be Issued or Temporary CO Issued, except Site Visit Inspection
+if (inspType != "Site Visit" && (!wasCapStatus(["Issued","Temporary CO Issued"]))) {
         showMessage = true;
         comment('<font size=small><b>Record must be Issued to schedule inspections</b></font>');
         cancel = true;
