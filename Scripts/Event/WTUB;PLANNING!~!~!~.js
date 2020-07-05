fce35167381afa8c4961ffd484d94c8ace24b53e
bushatos <66446858+bushatos@users.noreply.github.com>
2020-07-05 14:45:50 -0400

    Add Planning Condition Check

---
+++ Scripts/Event/WTUB;PLANNING!~!~!~.js
@@ -0,0 +1,40 @@
+/*
+Purpose : checking for conditions on parcels at each record type
+Description : 
+ */
+//Constants:
+
+try {
+	//Manufactured Homes and RPA Exception Record Types and Zoning Case - !appMatch(Planning/LandUse/ZoningCase/NA) ??
+	if (matches(wfTask, 'BOS Hearing') && matches(wfStatus, 'Approved') && 
+		(getParcelConditions('BI', 'Applied', null, null) ||
+		getParcelConditions('CDOT', 'Applied', null, null) ||
+		getParcelConditions('EE', 'Applied', null, null) ||
+		getParcelConditions('Fire', 'Applied', null, null) ||
+		getParcelConditions('Health', 'Applied', null, null) ||
+		getParcelConditions('Parks and Rec', 'Applied', null, null) ||
+		getParcelConditions('Planning', 'Applied', null, null) ||
+		getParcelConditions('Utilities', 'Applied', null, null) ||
+		getParcelConditions('VDOT', 'Applied', null, null) ||
+		getParcelConditions('Real Property', 'Applied', null, null))) {
+		showMessage = true;
+		comment('The Parcel(s) seem to have still applied Conditions? You will need to update the Condition Status to Condition Met to proceed in the workflow');
+		cancel = true;
+	}
+	//Variance and SpecialException and AdminVariance
+	if (matches(wfTask, 'Case Complete') && matches(wfStatus, 'Closed') && 
+		(getParcelConditions('CDOT', 'Applied', null, null) ||
+		getParcelConditions('EE', 'Applied', null, null) ||
+		getParcelConditions('Fire', 'Applied', null, null) ||
+		getParcelConditions('Health', 'Applied', null, null) ||
+		getParcelConditions('Parks and Rec', 'Applied', null, null) ||
+		getParcelConditions('Planning', 'Applied', null, null) ||
+		getParcelConditions('Utilities', 'Applied', null, null) ||
+		getParcelConditions('VDOT', 'Applied', null, null))) {
+		showMessage = true;
+		comment('The Parcel(s) seem to have still applied Conditions? You will need to update the Condition Status to Condition Met to proceed in the workflow');
+		cancel = true;
+	}
+} catch (err) {
+	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
--- Scripts/Misc/ScriptTester.js
+++ Scripts/Misc/ScriptTester.js
@@ -1,14 +1,10 @@
-//var myCapId = "20170103-00001"; 
-//var myCapId = "17PR0005"; 
-//var myCapId = "17CP0007";
-//var myCapId = "17ES0003";
-var myCapId = "ELE2017-00002";
-var myUserId = "DOMINGO";
+var myCapId = "20SN0025";
+var myUserId = "DBOUCHER";
 
 eventName = "";
 /* ASA  */ var eventName = "ApplicationSubmitAfter";
 /* ASIUA */ //var eventName = "ApplicationSpecificInfoUpdateAfter";
-/* WTUA */  //var eventName = "WorkflowTaskUpdateAfter";  wfTask = "Plan Revisions";	  wfStatus = "Ready to Issue";  wfDateMMDDYYYY = "08/31/2016";
+/* WTUA */  //var eventName = "WorkflowTaskUpdateAfter";  wfTask = "BOS Hearing";	  wfStatus = "Approved";  wfDateMMDDYYYY = "06/27/2020";
 /* IRSA */  //var eventName = "InspectionResultSubmitAfter" ; inspResult = "Pass"; inspResultComment = "Comment";  inspType = "Check Job Status"
 /* ISA  */  //var eventName = "InspectionScheduleAfter" ; inspType = "Roofing"
 /* PRA  */  //var eventName = "PaymentReceiveAfter";  
@@ -16,12 +12,8 @@ eventName = "";
 var useProductScript = false;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
 var runEvent = true; // set to true to simulate the event and run all std choices/scripts for the record type.  
 
-/* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
+/* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions();  
 
-//
-// User code goes here
-//
-//
 
 // fix the logDebug issue.
 var original_func = logDebug;
@@ -36,6 +28,7 @@ try {
         // FeeItemsQuantityList = new java.lang.String("[1]"); 
         // NumberOfFeeItems = 1;
 		showDebug = true;
+		addParcelStdCondition(parcelNum, cType, cDesc, cComment)
 		// aa.print("Application Expiration Date: " + dateAdd(fileDate,180));
 		// aa.print("Balance due: " + balanceDue);
 		// aa.print("Balance due: " + currencyFormat(balanceDue));
@@ -49,81 +42,12 @@ try {
         //         "Right of Way");
         //     calcWFTDueDate(taskList, dueDays, fileDate, true);
         // }    
-        // 
-
-        //documentScripts();
-        //getEnvValues();
-        // var feesArray = loadFees();
-        // var feeItemsArray = new Array;
-        // for (f in feesArray) {
-        //     var fee = feesArray[f];
-        //     feeItemsArray.push(fee.code.toString());
-        // }
-        // documentObject(feeItemsArray);
-
-        // for (i in feeItemsArray) {
-        //     aa.print(feeItemsArray[i]);
-        // }
-        //feeAssess2();
-        //documentObject(sysDate, true);
-        //calcFee_CC_LU_ZC_CC_LU_ZC_03();
-
-        // logDebug("<m"+"eta charset='utf-8'>");
-        // logDebug("<m"+"eta name='viewport' content='width=device-width, initial-scale=1'>");
-        // logDebug("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>");
-        // logDebug("<s" + "cript src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
-        // logDebug("<s" + "cript src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>");
-        // logDebug("<div class='container'>");
-        // logDebug("<h1>Bootstrap Section</h1>");
-        // logDebug("<div class='row'>");
-        // logDebug("<div class='col-sm-4' style='background-color:lavender;''>");
-        // logDebug("<a href='#'>Skip to main content</a>");
-        // logDebug("</div>");
-        // logDebug("<div class='col-sm-8'>");
-        // logDebug("<p>More text here.</p>");
-        // logDebug("</div>");
-        // logDebug("</div>");
-        // 
-        // var ctl = aa.cap.getCapTypeList(null).getOutput();
-        // for (i in ctl) {
-        //     documentObject(ctl[i]);
-        // }
- 
-        // documentObject(systemUserObj);
-        // aa.print("currentUser rate: " + systemUserObj.getRate1()); 
-
-        // var UserID = "DDEJESUS";
-        // var systemUserObjResult = aa.person.getUser(UserID.toUpperCase());
-
-        // if (systemUserObjResult.getSuccess()) {
-        //     var systemUserObj = systemUserObjResult.getOutput();
-        //     //documentObject(systemUserObj);
-        //     //for(x in systemUserObj)
-        //     //aa.print("systemUserObj:" + x);
-
-        //     aa.print("Assigned User Full Name:" + systemUserObj.getFullName());
-        //     aa.print("getBillingRate:" + systemUserObj.getRate1());
-        //     aa.print("class " + systemUserObj.class);
-
-        // }
-
-        //documentObject(aa.finance, true);
-        
-        // var tree = new Tree();
-        // for (var i = 0; i < 10; i++) {
-        //     tree.addValue(Math.floor((Math.random() * 100) + 1));
-        // }
-        // tree.traverse();
-
 	}
 catch (err) {
 	aa.print("A JavaScript Error occured: " + err.message);
 	}
-// end user code
-//aa.env.setValue("ScriptReturnCode", "1"); 	aa.env.setValue("ScriptReturnMessage", debug)
-//
-//
-
+	var z = debug.replace(/<BR>/g,"\r");  aa.print(z);
+	
 function getEnvValues() {
     var k = aa.env.getParamValues();
     var em = k.keys();
@@ -240,11 +164,39 @@ function currencyFormat (num) {
     return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
 }
 
-// function logDebug(dstr) {
-//     if (showDebug) {
-//         aa.print(dstr)
-//             //emailText += dstr + "<br>";
-//             //aa.debug(aa.getServiceProviderCode() + " : " + aa.env.getValue("CurrentUserID"), dstr);
-//     }
-// }
+function addParcelStdCondition(parcelNum, cType, cDesc, cComment)
+//if parcelNum is null, condition is added to all parcels on CAP
+{
+    if (!parcelNum) {
+        var capParcelResult = aa.parcel.getParcelandAttribute(capId, null);
+        if (capParcelResult.getSuccess()) {
+            var Parcels = capParcelResult.getOutput().toArray();
+            for (zz in Parcels) {
+				logDebug("Adding Standard Condition to parcel #" + zz + " = " + Parcels[zz].getParcelNumber());
+				//standardConditions = aa.parcelCondition.getStandardConditions(cType, cDesc).getOutput();
+				standardConditions = aa.capCondition.getStandardConditions(cType,cDesc).getOutput();
+				for (i = 0; i < standardConditions.length; i++) {
+					standardCondition = standardConditions[i]
+						var addCapCondResult = aa.parcelCondition.addParcelCondition(Parcels[zz].getParcelNumber(), standardCondition.getConditionType(), standardCondition.getConditionDesc(), cComment, sysDate, null, sysDate, 
+							null, null, standardCondition.getImpactCode(), systemUserObj, systemUserObj, "Applied", currentUserID, "A", null, standardCondition.getDisplayConditionNotice(), standardCondition.getIncludeInConditionName(),
+							standardCondition.getIncludeInShortDescription(), standardCondition.getInheritable(), standardCondition.getLongDescripton(), standardCondition.getPublicDisplayMessage(), standardCondition.getResolutionAction(),
+							null, null, standardCondition.getConditionNbr(), standardCondition.getConditionGroup(), standardCondition.getDisplayNoticeOnACA(), standardCondition.getDisplayNoticeOnACAFee(), standardCondition.getPriority(), standardCondition.getConditionOfApproval());
+						if (addCapCondResult.getSuccess()) {
+							logDebug("Successfully added condition (" + standardCondition.getConditionDesc() + ")");
+						} else {
+							logDebug("**ERROR: adding condition (" + standardCondition.getConditionDesc() + "): " + addCapCondResult.getErrorMessage());
+						}
+				}
+			}
+		} else {
+        var addParcelCondResult = aa.parcelCondition.addParcelCondition(parcelNum, cType, cDesc, cComment, null, null, cImpact, cStatus, sysDate, null, sysDate, sysDate, systemUserObj, systemUserObj);
 
+        if (addParcelCondResult.getSuccess()) {
+            logMessage("Successfully added condition to Parcel " + parcelNum + "  (" + cImpact + ") " + cDesc);
+            logDebug("Successfully added condition to Parcel " + parcelNum + "  (" + cImpact + ") " + cDesc);
+        } else {
+            logDebug("**ERROR: adding condition to Parcel " + parcelNum + "  (" + cImpact + "): " + addParcelCondResult.getErrorMessage());
+        }
+		}
+	}
+}
