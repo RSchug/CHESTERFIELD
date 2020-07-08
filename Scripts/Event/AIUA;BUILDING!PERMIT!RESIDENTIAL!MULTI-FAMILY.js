96e731cc09d4532a5e612b11d1bfb677a6eb3c0b
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-08 12:45:38 -0400

    State Levy as addFee

--- Scripts/Event/AIUA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
+++ Scripts/Event/AIUA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
@@ -1,3 +1,3 @@
 //Update Fees if additional information is updated//
 updateFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",houseCount,"Y");
-updateFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
+addFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
