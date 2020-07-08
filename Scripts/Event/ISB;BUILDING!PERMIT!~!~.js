26629cb58793f064854d3fe93d57eab820646a44
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-08 13:39:43 -0400

    removeInspection

--- Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
@@ -4,4 +4,5 @@ if (inspType != "Site Visit" && (!wasCapStatus(["Issued","Temporary CO Issued"])
         showMessage = true;
         comment('<font size=small><b>Record must be Issued to schedule inspections</b></font>');
         cancel = true;
+        removeInspection(inspObj); 
 }
