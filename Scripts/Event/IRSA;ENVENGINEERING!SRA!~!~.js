e405acdf1f052e67e171408e760fde2b2c82d548
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-11 12:15:48 -0400

    Added Variable for InspAssignment

--- Scripts/Event/IRSA;ENVENGINEERING!SRA!~!~.js
+++ Scripts/Event/IRSA;ENVENGINEERING!SRA!~!~.js
@@ -1,3 +1,6 @@
+//Variables for the EE Inspector based on Parcel field "Inspection Dist" and Standard Choice 'InspectionAssignmentEnvEngineering'
+var ParcelInspectorEnvEng = AInfo["ParcelAttribute.INSPECTION DIST"];
+var InspAssignment = lookup("InspectionAssignmentEnvEngineering",ParcelInspectorEnvEng);
 //If EE QC Inspection Result is 'Approved' then close Inspections Workflow Task.//
 if (inspType.equals("EE QC Inspection") && inspResult.equals("Approved")){
 closeTask("Inspections","Approved","Updated based on Approved Inspection Result","");
@@ -8,13 +11,13 @@ updateTask("Inspections","Not Approved","Updated based on Not Approved Inspectio
 }
 //If VDOT Inspection Result is 'Approved' then schedule Utilities Inspection for next business day.//
 if (inspType.equals("VDOT Inspection") && inspResult.equals("Approved")){
-scheduleInspection("Utilities Inspection",1,currentUserID,null,"Auto Scheduled");
+scheduleInspection("Utilities Inspection",1,InspAssignment,null,"Auto Scheduled");
 }
 //If Utilities Inspection Result is 'Approved' then schedule EE Inspection for next business day.//
 if (inspType.equals("Utilities Inspection") && inspResult.equals("Approved")){
-scheduleInspection("EE Inspection",1,currentUserID,null,"Auto Scheduled");
+scheduleInspection("EE Inspection",1,InspAssignment,null,"Auto Scheduled");
 }
 //If EE Inspection Result is 'Approved' then schedule EE QC Inspection for next business day.//
 if (inspType.equals("EE Inspection") && inspResult.equals("Approved")){
-scheduleInspection("EE QC Inspection",1,currentUserID,null,"Auto Scheduled");
+scheduleInspection("EE QC Inspection",1,InspAssignment,null,"Auto Scheduled");
 }
