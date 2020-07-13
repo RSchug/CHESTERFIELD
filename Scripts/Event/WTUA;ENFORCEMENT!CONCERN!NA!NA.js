ecdeae31cf50c4c60f4fc8a4581a2e9bebe923da
Ray Schug <RSchug@TruePointSolutions.com>
2020-07-13 16:07:05 -0700

    1CE - 3CE Copy ASI to Child

--- Scripts/Event/WTUA;ENFORCEMENT!CONCERN!NA!NA.js
+++ Scripts/Event/WTUA;ENFORCEMENT!CONCERN!NA!NA.js
@@ -6,53 +6,63 @@
         copy Custom Fields Data from Parent record that is checked to the Inspection Request field on the Child Record
         CC-ENF-ZONE >  CC-ENF-PROAC or CC-ENF-ZONE
 */
-if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required PM') 
-{   deactivateTask("Community Enhancement"); 
+if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required PM') {
+    deactivateTask("Community Enhancement");
     newChildID = createChild("Enforcement","Property Maintenance","NA","NA","");
-    saveCapId = capId;
-    capId = newChildID;
-    get
-    scheduleInspection_CHESTERFIELD("Initial",1,null,null,"Auto Scheduled");
-    capId = saveCapId;
-    copyAppSpecific(newChildID);
+    if (newChildID) {
+        saveCapId = capId;
+        capId = newChildID;
+        get
+        scheduleInspection_CHESTERFIELD("Initial",1,null,null,"Auto Scheduled");
+        capId = saveCapId;
+        copyAppSpecific(newChildID);
     }
+}
 
-if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required ZC') 
-{   deactivateTask("Community Enhancement"); 
-    deactivateTask("Initiation");
-    activateTask("Investigation");
+if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required ZC') {
+    deactivateTask("Community Enhancement");
     newChildID = createChild("Enforcement","Zoning Code Compliance","NA","NA","");
-    saveCapId = capId;
-    capId = newChildID;
-    scheduleInspection_CHESTERFIELD("Initial",1,null,null,"Auto Scheduled");
-    capId = saveCapId;
-    copyAppSpecific(newChildID);
+    if (newChildID) {
+        saveCapId = capId;
+        capId = newChildID;
+        scheduleInspection_CHESTERFIELD("Initial",1,null,null,"Auto Scheduled");
+        deactivateTask("Initiation");
+        activateTask("Investigation");
+        capId = saveCapId;
+        copyAppSpecific(newChildID);
     }
+}
 
-if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required PM and ZC') 
-{   deactivateTask("Community Enhancement"); 
+if (wfTask == 'Community Enhancement' && wfStatus == 'Inspection Required PM and ZC') {
+    deactivateTask("Community Enhancement");
     newChildID = createChild("Enforcement","Property Maintenance","NA","NA","");
-    saveCapId = capId;
-    capId = newChildID;
-    scheduleInspection_CHESTERFIELD("Initial",1,currentUserID,null,"Auto Scheduled");
-    capId = saveCapId;
-    copyAppSpecific(newChildID);
-    deactivateTask("Initiation");
-    activateTask("Investigation");
+    if (newChildID) {
+        saveCapId = capId;
+        capId = newChildID;
+        scheduleInspection_CHESTERFIELD("Initial",1,currentUserID,null,"Auto Scheduled");
+        capId = saveCapId;
+        copyAppSpecific(newChildID);
+    }
     newChildID = createChild("Enforcement","Zoning Code Compliance","NA","NA","");
-    saveCapId = capId;
-    capId = newChildID;
-    scheduleInspection_CHESTERFIELD("Initial",1,currentUserID,null,"Auto Scheduled");
-    capId = saveCapId;
-    copyAppSpecific(newChildID);
-    }
+    if (newChildID) {
+        saveCapId = capId;
+        capId = newChildID;
+        scheduleInspection_CHESTERFIELD("Initial", 1, currentUserID, null, "Auto Scheduled");
+        deactivateTask("Initiation");
+        activateTask("Investigation");
+        capId = saveCapId;
+        copyAppSpecific(newChildID);
 
-if (wfTask == 'Community Enhancement' && wfStatus == 'KCB Workorder')
-{   newChildID = createChild("Enforcement","KCB Workorder","NA","NA","");
-    saveCapId = capId;
-    capId = newChildID;
-    updateTask("Initiation","Pending Review","Updated based on Concern Record","");
-    capId = saveCapId;
-    copyAppSpecific(newChildID);
     }
-    
+}
+
+if (wfTask == 'Community Enhancement' && wfStatus == 'KCB Workorder'){
+    newChildID = createChild("Enforcement","KCB Workorder","NA","NA","");
+    if (newChildID) {
+        saveCapId = capId;
+        capId = newChildID;
+        updateTask("Initiation","Pending Review","Updated based on Concern Record","");
+        capId = saveCapId;
+        copyAppSpecific(newChildID);
+    }
+}
