dd5334086742bbcf2463367aea1d817fb56d9608
bushatos <66446858+bushatos@users.noreply.github.com>
2020-07-15 00:00:10 -0400

    clean up

--- INCLUDES_CUSTOM/parcelHasConditiontrue_TPS.js
+++ INCLUDES_CUSTOM/parcelHasConditiontrue_TPS.js
@@ -2,33 +2,18 @@ function parcelHasConditiontrue_TPS(pType,pStatus)
 // for the Parcel Conditions, checks all parcels, and if any have an Applied Condition, returns true
 {
 	var capParcelResult = aa.parcel.getParcelandAttribute(capId,null);
-	if (capParcelResult.getSuccess()) {
-		var Parcels = capParcelResult.getOutput().toArray();
-		for (zz in Parcels) {
-			logDebug("Getting Applied Conditions on parcel #" + zz + " = " + Parcels[zz].getParcelNumber());
-			var condResult = aa.parcelCondition.getParcelConditions(pType,pStatus,null,null);
-			if (condResult.getSuccess()) {
-				var capConds = condResult.getOutput();
-			} else { 
-				logDebug("**ERROR: getting parcel conditions: " + condResult.getErrorMessage());
-				return false;
-			}
+	if (!capParcelResult.getSuccess())
+		{ logDebug("**WARNING: error getting cap parcels : " + capParcelResult.getErrorMessage()) ; return false }
+
+	var Parcels = capParcelResult.getOutput().toArray();
+	for (zz in Parcels)
+		{
+		pcResult = aa.parcelCondition.getParcelConditions(Parcels[zz].getParcelNumber());
+		if (!pcResult.getSuccess())
+			{ logDebug("**WARNING: error getting parcel conditions : " + pcResult.getErrorMessage()) ; return false }
+		pcs = pcResult.getOutput();
+		for (pc1 in pcs)
+			if (pcs[pc1].getConditionType().equals(pType) && pcs[pc1].getConditionStatus().equals(pStatus)) 
+				return true;
 		}
-	}
-	var cStatus;
-	var cDesc;
-	for (cc in capConds) {
-		var thisCond = capConds[cc];
-		var cStatus = thisCond.getConditionStatus();
-		var cDesc = thisCond.getConditionDescription();
-		if (cStatus==null)
-			cStatus = " ";
-		if (cDesc==null)
-			cDesc = " ";
-		logDebug("The Status of the " + cDesc + " Condition is " + cStatus);
-		//Look for matching condition
-		if ( (pStatus==null || pStatus.toUpperCase().equals(cStatus.toUpperCase())) && (pDesc==null || pDesc.toUpperCase().equals(cDesc.toUpperCase())) )
-			return true; //matching condition found
-	}
-	return false; //no matching condition found
 }
