94b0cb2fc1c3cd7f5de2b1a60ea6e6ed77424aeb
Ray Schug <RSchug@TruePointSolutions.com>
2020-07-15 17:08:46 -0700

    Elevator

---
+++ INCLUDES_CUSTOM/copyDocuments.js
@@ -0,0 +1,89 @@
+function copyDocuments(pFromCapId, pToCapId) {
+    if (pToCapId == null)
+        var vToCapId = capId;
+    else
+        var vToCapId = pToCapId;
+	//Copies all attachments (documents) from srcCapId to targetCapId
+	var docCategories = null;
+	if (arguments.list > 2) docCategories = arguments[2];
+	var docStatuses = null;
+	if (arguments.list > 3) docStatuses = arguments[3];
+	var preventDuplicate = true;
+	if (arguments.list > 4 && arguments[4] != null) preventDuplicate = arguments[4];
+	var newDocStatus = null;
+	if (arguments.list > 5) newDocStatus = arguments[5];
+
+	var capDocResult = aa.document.getDocumentListByEntity(pFromCapId, "CAP");
+	if (capDocResult.getSuccess()) {
+		if (capDocResult.getOutput().size() > 0) {
+			for (docInx = 0; docInx < capDocResult.getOutput().size(); docInx++) {
+				var documentObject = capDocResult.getOutput().get(docInx);
+				if (docCategories && !exists(documentObject.getDocCategory(),docCategories)) continue;
+				if (docStatuses && !exists(documentObject.getDocStatus(),docStatuses)) continue;
+
+                // Check for Duplicate Document
+                documentFound = false;
+                if (!preventDuplicate) {
+                    var capDocuments2 = aa.document.getDocumentListByEntity(vToCapId, "CAP");
+                    if (capDocuments2.getSuccess()) {
+                        var capDocumentList2 = capDocuments2.getOutput();
+                        if (capDocumentList2.size() > 0) {
+                            for (index = 0; index < capDocumentList2.size(); index++) {
+                                capDocumentModel2 = capDocumentList2.get(index);
+                                if (capDocumentModel.getDocName() == capDocumentModel2.getDocName() && capDocumentModel.getFileKey() == capDocumentModel2.getFileKey()) {
+                                    logDebug("Skipping document1: " + capDocumentModel.getDocumentNo() + " " + capDocumentModel.getDocName() + " " + capDocumentModel.getFileKey() + " from " + pFromCapId.getCustomID() + " to " + vToCapId.getCustomID());
+                                    documentFound = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (documentFound) continue;
+				
+				// download the document content
+				var useDefaultUserPassword = true;
+				//If useDefaultUserPassword = true, there is no need to set user name & password, but if useDefaultUserPassword = false, we need define EDMS user name & password.
+				var EMDSUsername = null;
+				var EMDSPassword = null;
+
+				var downloadResult = aa.document.downloadFile2Disk(documentObject, documentObject.getModuleName(), EMDSUsername, EMDSPassword, useDefaultUserPassword);
+				if (downloadResult.getSuccess()) {
+					var path = downloadResult.getOutput();
+					logDebug("path=" + path);
+				}
+
+				var tmpEntId = vToCapId.getID1() + "-" + vToCapId.getID2() + "-" + vToCapId.getID3();
+				documentObject.setDocumentNo(null);
+				documentObject.setCapID(vToCapId);
+				documentObject.setEntityID(tmpEntId);
+
+				// Open and process file
+				try {
+					// put together the document content - use java.io.FileInputStream
+					var newContentModel = aa.document.newDocumentContentModel().getOutput();
+					inputstream = new java.io.FileInputStream(path);
+					newContentModel.setDocInputStream(inputstream);
+					documentObject.setDocumentContent(newContentModel);
+
+					var newDocResult = aa.document.createDocument(documentObject);
+					if (newDocResult.getSuccess()) {
+					    var newDocObject = newDocResult.getOutput();
+						if (newDocStatus) {
+							newDocObject.setDocStatus(newDocStatus);
+							aa.document.updateDocument(newDocObject);
+						}
+						newDocResult.getOutput();
+						logDebug("Successfully copied document: " + documentObject.getFileName());
+					} else {
+						logDebug("Failed to copy document: " + documentObject.getFileName());
+						logDebug(newDocResult.getErrorMessage());
+					}
+				} catch (err) {
+					logDebug("Error copying document: " + err.message);
+					return false;
+				}
+
+			}
+		}
+	}
+}
