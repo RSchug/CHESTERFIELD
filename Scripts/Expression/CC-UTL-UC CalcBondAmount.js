b2d5bb2e3f30c3e06222c97f04c1e45a5e513336
Ray Schug <RSchug@TruePointSolutions.com>
2020-07-16 15:54:09 -0700

    CalcBondAmount

--- Scripts/Expression/CC-UTL-UC CalcBondAmount.js
+++ Scripts/Expression/CC-UTL-UC CalcBondAmount.js
@@ -245,12 +245,13 @@ try {
         commercialProject = false;
 
     // For Utility Construction Project calculate Bond Amount
-    /*
-    if (stateRoadAcceptDate.getTime() > minValue.getTime()) {
+    var stateRoadAcceptStatus = taskStatus("Utility State Road Acceptance Inspection");
+    logDebug("stateRoadAcceptStatus: "+ stateRoadAcceptStatus);
+    //if (stateRoadAcceptDate.getTime() > minValue.getTime()) {
+    if (stateRoadAcceptStatus == "Completed") {
         bondTotalSewer = 0;
         bondTotalWater = 0;
-    } else { */
-    if (true) {
+    } else {
         if (percentCompleteSewer >= 90)
             bondTotalSewer = estCostSewer * 0.1
         else
@@ -488,6 +489,36 @@ function getAppSpecific(itemName, itemCap) {
     else { logDebug("**ERROR: getting app specific info for Cap : " + appSpecInfoResult.getErrorMessage()) }
 }
 
+function taskStatus(wfstr) // optional process name and capID
+{
+    var useProcess = false;
+    var processName = "";
+    var itemCap = capId;
+    if (arguments.length >= 2) {
+        processName = arguments[1]; // subprocess
+        if (processName)
+            useProcess = true;
+    }
+
+    if (arguments.length == 3)
+        itemCap = arguments[2]; // use cap ID specified in args
+
+
+    var workflowResult = aa.workflow.getTaskItems(itemCap, wfstr, processName, null, null, null);
+    if (workflowResult.getSuccess())
+        var wfObj = workflowResult.getOutput();
+    else {
+        logMessage("**ERROR: Failed to get workflow object: " + workflowResult.getErrorMessage());
+        return false;
+    }
+
+    for (i in wfObj) {
+        var fTask = wfObj[i];
+        if (fTask.getTaskDescription().toUpperCase().equals(wfstr.toUpperCase()) && (!useProcess || fTask.getProcessCode().equals(processName)))
+            return fTask.getDisposition()
+    }
+} 
+
 /*------------------- Standard Expression Functions --------------------- */
 var toPrecision = function (value) {
     if (value == null) return 0;
@@ -516,3 +547,4 @@ function formatDate(dateString, pattern) {
     }
     return expression.formatDate(dateString, pattern);
 }
+
