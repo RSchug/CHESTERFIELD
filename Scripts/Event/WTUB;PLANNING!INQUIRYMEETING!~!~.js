0a5c775b759db777341d094c54a797bd5527efe4
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-19 16:35:53 -0400

    wrong Workflow Task Name in Business Rule

--- Scripts/Event/WTUB;PLANNING!INQUIRYMEETING!~!~.js
+++ Scripts/Event/WTUB;PLANNING!INQUIRYMEETING!~!~.js
@@ -1,7 +1,7 @@
 //19P IF Workflow Task = Review Coordination and Status is updated, if Data Field 'Minor Site Plan Allowed' is null, 
 //THEN show Message 'Approved for Minor Site data needs to be populated.'
 try {
-	if (wfTask == 'Review Coordination' &&  (AInfo["11. Minor Site Plan Allow"] == null)) {
+	if (wfTask == 'Review Consolidation' &&  (AInfo["11. Minor Site Plan Allow"] == null)) {
         showMessage = true;
         comment('<font size=small><b>Approved for Minor Site data needs to be populated');
         cancel = true;
