f7613712376d2dbce933b8c373f78af599962269
bushatos <66446858+bushatos@users.noreply.github.com>
2020-07-19 09:14:37 -0400

    clean up

--- Scripts/Event/ASUA;PLANNING!LANDUSE!LANDDISTURBANCE!~.js
+++ Scripts/Event/ASUA;PLANNING!LANDUSE!LANDDISTURBANCE!~.js
@@ -1,3 +1,8 @@
-if(matches(appStatus,"Cancelled","Completed","Expired","Withdrawn"))
-{inspCancelAll();
-}?
+try {
+	//?
+	if(matches(appStatus,"Cancelled","Completed","Expired","Withdrawn")) {
+		inspCancelAll();
+	}
+} catch (err) {
+	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
