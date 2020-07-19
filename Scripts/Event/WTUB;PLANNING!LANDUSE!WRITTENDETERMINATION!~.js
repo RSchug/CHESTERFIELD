6c64598dca46e5ba2defd12150fe7ccbd90a12c3
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-19 17:01:39 -0400

    fix

--- Scripts/Event/WTUB;PLANNING!LANDUSE!WRITTENDETERMINATION!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!WRITTENDETERMINATION!~.js
@@ -11,5 +11,3 @@ try {
 {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
-
-if(matches(inspType,"Pre-Construction Meeting"))
