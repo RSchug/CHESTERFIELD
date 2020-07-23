d32e653a09116aff15fffb7b2e6622354bc8c44a
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-23 18:29:25 -0400

    ainfo

--- Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
@@ -1,3 +1,3 @@
 //SITE PLAN FEE
 if (!feeExists("SITEPLAN")){
-    addFee("SITEPLAN","CC-PLANNING","FINAL",1,"N")}
+    addFee("SITEPLAN","CC-PLANNING","FINAL",(AInfo["Total application acreage"]-1),"N")}
