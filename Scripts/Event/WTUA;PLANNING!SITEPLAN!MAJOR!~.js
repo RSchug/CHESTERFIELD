c080ba18cb4746e5d5b72e2c0c2fec0dccf2d299
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-23 18:33:26 -0400

    undo

--- Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
@@ -1,3 +1,4 @@
 //SITE PLAN FEE
 if (!feeExists("SITEPLAN")){
-    addFee("SITEPLAN","CC-PLANNING","FINAL",(AInfo["Total application acreage"]-1),"N")}
+    addFee("SITEPLAN","CC-PLANNING","FINAL",1,"N")}
+    //    addFee("SITEPLAN","CC-PLANNING","FINAL",(AInfo["Total application acreage"]-1),"N")}
