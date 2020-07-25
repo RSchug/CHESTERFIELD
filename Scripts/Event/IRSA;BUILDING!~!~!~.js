da183602a5143163c548eacbb836bc0a49360497
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-24 20:29:21 -0400

    building final

--- Scripts/Event/IRSA;BUILDING!~!~!~.js
+++ Scripts/Event/IRSA;BUILDING!~!~!~.js
@@ -11,9 +11,15 @@ if (matches(inspResult, "Corrections Required") && inspOvertime == "Y") {
 	addFeeWithExtraData("NOTREADY", "CC-BLD-ADMIN", "FINAL", 1, "Y", capId, inspType);
 }
 //If Inspection Result is "Approved" for Inspection Type "Building Final" close the Inspections Workflow Task.//
-if (inspType.equals("Building Final") && inspResult.equals("Approved")){
+if (appMatch("Building/Permit/Residential/NA") && inspType.equals("Building Final") && inspResult.equals("Approved")){
+	updateTask("Inspections","CO Ready to Issue","Updated based on Completed Inspection Result","");
+	}
+if (appMatch("Building/Permit/Commercial/NA") && inspType.equals("Building Final") && inspResult.equals("Approved")){
 	closeTask("Inspections","CO Ready to Issue","Updated based on Completed Inspection Result","");
 	}
+if (appMatch("Building/Permit/Residential/Multi-Family") && inspType.equals("Building Final") && inspResult.equals("Approved")){
+	updateTask("Inspections","CO Ready to Issue","Updated based on Completed Inspection Result","");
+	}	
 //If Inspection Result is "Approved" for Inspection Type "Property Conversion Inspection" close the Inspections Workflow Task.//
 if (inspType.equals("Property Conversion Inspection") && inspResult.equals("Approved")){
 	closeTask("Inspections","Approved","Updated based on Completed Inspection Result","");
