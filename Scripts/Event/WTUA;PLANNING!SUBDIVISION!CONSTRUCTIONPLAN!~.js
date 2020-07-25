163bfbaa8658d02d42eeea57032bb919a5460c7e
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-25 16:55:44 -0400

    on ESC fee

--- Scripts/Event/WTUA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
+++ Scripts/Event/WTUA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
@@ -1,6 +1,4 @@
-//Site Plan - Initial Submittal Fee 8.1P
 if (wfTask == 'Application Submittal' && wfStatus == 'Accepted for First Glance Review') {
-    addFee("SITEPLAN","CC-PLANNING","FINAL",1,"N");
 //Erosion and Sediment Control Review and Enforcement Fees 8.2P
     addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
     addFee("ERSCRENFORCE","CC-PLANNING","FINAL",1,"N");
