ebe689364c58faa51b8079b6a316ce943048167d
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-25 15:57:09 -0400

    review verbiage change

--- Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
@@ -2,7 +2,7 @@
 if (wfTask == 'Application Submittal' && wfStatus == 'Accepted for First Glance Review') {
     addFee("SITEPLAN","CC-PLANNING","FINAL",1,"N")}
 //Erosion and Sediment Control Review and Enforcement Fees TBD on what determines Minimum or calculation
-if (wfTask == 'First Glance Consolidation' && wfStatus == 'First Glance Complete') {
+if (wfTask == 'First Glance Consolidation' && wfStatus == 'First Glance Review Complete') {
     addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
     addFee("ERSCRENFORCE","CC-PLANNING","FINAL",1,"N")}
 //Site Plan - Submittals Subsequent to First 3 Submittals Fees based on ASI Field 'Submittal Count'
