e62b3ca404e7fe97c1587305f08129d630181b81
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-25 15:48:18 -0400

    Fees

--- Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/WTUA;PLANNING!SITEPLAN!MAJOR!~.js
@@ -1,4 +1,10 @@
-//SITE PLAN FEE
-if (!feeExists("SITEPLAN")){
+//Site Plan - Initial Submittal Fee
+if (wfTask == 'Application Submittal' && wfStatus == 'Accepted for First Glance Review') {
     addFee("SITEPLAN","CC-PLANNING","FINAL",1,"N")}
-    //    addFee("SITEPLAN","CC-PLANNING","FINAL",(AInfo["Total application acreage"]-1),"N")}
+//Erosion and Sediment Control Review and Enforcement Fees TBD on what determines Minimum or calculation
+if (wfTask == 'First Glance Consolidation' && wfStatus == 'First Glance Completed') {
+    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+    addFee("ERSCRENFORCE","CC-PLANNING","FINAL",1,"N")}
+//Site Plan - Submittals Subsequent to First 3 Submittals Fees based on ASI Field 'Submittal Count'
+if ((wfTask == 'Review Distribution' && wfStatus == 'Revisions Received') && (AInfo["Submittal Count"] > 3)){
+    addFee("SITEPLAN2","CC-PLANNING","FINAL",1,"N")}
