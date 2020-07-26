569291a056feb3ab6fbc65f1178fe183f4813e64
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-26 15:17:36 -0400

    FEE

--- Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
@@ -11,3 +11,7 @@ if (wfTask =='Review Consolidation' && wfStatus == 'Move to BOS'){
 	closeTask("Review Consolidation", "Move to BOS", "","");
 	activateTask("BOS Staff Report");
 }
+//Fees for Substantial Accord
+if (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment'){
+	addFee("SAOTHER","CC-PLANNING","FINAL",1,"N");
+}
