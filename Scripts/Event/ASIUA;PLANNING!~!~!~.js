411c757993fecc9d6ed73bf9729807e21c8c4a75
bushatos <66446858+bushatos@users.noreply.github.com>
2020-07-27 12:26:13 -0400

    Updated for capitalizations in the data labels 57p

--- Scripts/Event/ASIUA;PLANNING!~!~!~.js
+++ Scripts/Event/ASIUA;PLANNING!~!~!~.js
@@ -3,7 +3,7 @@
 // 57P Summarize each row in the table data: CC - LU - TPA:
 // 'Parcel Acreage' to the Custom Field: 'Total Parcel Acreage'
 // 'Revised Acreage' to the Custom Field: 'Total application acreage'
-// Count the number of rows to the Custom Field: Total Number of parcels'
+// Count the number of rows to the Custom Field: Total number of parcels'
 // Copied from ASIUA:Planning/LANDUSE/*/*
 
 var tempAsit = loadASITable("CC-LU-TPA");
@@ -12,16 +12,16 @@ if (tempAsit) {
     var revisedAcreage = 0;
     var countParcels = 0;
 	for (a in tempAsit) {
-        if (!isNaN(tempAsit[a]["Parcel acreage"])) {
-            parcelAcreage += parseInt(tempAsit[a]["Parcel acreage"]);
+        if (!isNaN(tempAsit[a]["Parcel Acreage"])) {
+            parcelAcreage += parseInt(tempAsit[a]["Parcel Acreage"]);
         }
-        if (!isNaN(tempAsit[a]["Revised acreage"])) {
-            revisedAcreage += parseInt(tempAsit[a]["Revised acreage"]);
+        if (!isNaN(tempAsit[a]["Revised Acreage"])) {
+            revisedAcreage += parseInt(tempAsit[a]["Revised Acreage"]);
         }
         countParcels++;
 	}//for all rows
 	//editAppSpecific("CC-LU-TPA-SUM.Total application acreage", sum);
     editAppSpecific("Total Parcel Acreage", parcelAcreage);
     editAppSpecific("Total application acreage", revisedAcreage);
-    editAppSpecific("Total Number of parcels", countParcels);
+    editAppSpecific("Total number of parcels", countParcels);
 }
