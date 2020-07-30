b6931ac0c1fdf19895285677a4176f05c3764da4
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-30 18:53:39 -0400

    FEE

--- Scripts/Event/WTUA;PLANNING!SUBDIVISION!OVERALLCONCEPTUALPLAN!~.js
+++ Scripts/Event/WTUA;PLANNING!SUBDIVISION!OVERALLCONCEPTUALPLAN!~.js
@@ -1,4 +1,5 @@
 //FEE
 if (wfTask == 'First Glance Consolidation' && wfStatus == 'First Glance Review Complete') {
     addFee("OVERALLPLAN","CC-PLANNING","FINAL",1,"N");
+    addFee("OCPLAN","CC-PLANNING","FINAL",1,"N");
 }
