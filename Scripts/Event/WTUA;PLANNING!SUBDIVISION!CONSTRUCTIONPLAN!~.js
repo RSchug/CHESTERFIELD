c7802d66c75adba2a0a7cfc58c4fc913d5a652d5
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-07-30 18:12:49 -0400

    FEE

--- Scripts/Event/WTUA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
+++ Scripts/Event/WTUA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
@@ -1,4 +1,4 @@
 //Erosion and Sediment Control Review and Enforcement Fees 8.2P and 8.3P
 if ((wfTask == 'First Glance Consolidation' && wfStatus == 'First Glance Review Complete') && (AInfo["Total Residential Lots"] != null)) {
-    addFee("ERSCRENFORCE","CC-PLANNING","FINAL",1,"N"); 
+    addFee("ERSCRENFRLOT","CC-PLANNING","FINAL",1,"N"); 
 } 
