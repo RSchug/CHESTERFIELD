fc18d7a87c20b667299bb7fc0555b4134a91f6c5
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-05 13:45:04 -0400

    update

--- Scripts/Event/WTUA;ENVENGINEERING!~!~!~.js
+++ Scripts/Event/WTUA;ENVENGINEERING!~!~!~.js
@@ -6,6 +6,6 @@ if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEn
 if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEngineering/Plan Review/ESC Plan/NA')) && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage >.229)) {
     addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
     }
-    if (appMatch('EnvEngineering/Plan Review/Timbering/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment')) {
-        addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
-        }
+if (appMatch('EnvEngineering/Plan Review/Timbering/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment')) {
+    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+    }
