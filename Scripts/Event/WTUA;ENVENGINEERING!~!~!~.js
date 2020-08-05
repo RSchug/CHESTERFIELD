7f481da6306096bdb876599e7b2f76766a15657e
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-05 14:07:15 -0400

    update

--- Scripts/Event/WTUA;ENVENGINEERING!~!~!~.js
+++ Scripts/Event/WTUA;ENVENGINEERING!~!~!~.js
@@ -1,11 +1,17 @@
 //Fees
 var TotalLDAcreage = parseFloat(AInfo['Total Land Disturbance Acreage']);
-if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEngineering/Plan Review/ESC Plan/NA')) && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage <=.229)) {
-    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+if (appMatch('EnvEngineering/Plan Review/Linear Project/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage <=.229)) {
+    addFee("ERSCRENFMIN","CC-EE","FINAL",1,"Y");
     }
-if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEngineering/Plan Review/ESC Plan/NA')) && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage >.229)) {
-    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+if (appMatch('EnvEngineering/Plan Review/ESC Plan/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage <=.229)) {
+    addFee("ERSCRENFMIN","CC-EE","FINAL",1,"Y");
+    }
+if (appMatch('EnvEngineering/Plan Review/Linear Project/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage >.229)) {
+    addFee("ERSCRENFORCE","CC-EE","FINAL",1,"Y");
+    }
+if (appMatch('EnvEngineering/Plan Review/ESC Plan/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage >.229)) {
+    addFee("ERSCRENFORCE","CC-EE","FINAL",1,"Y");
     }
 if (appMatch('EnvEngineering/Plan Review/Timbering/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment')) {
-    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+    addFee("ERSCRENFMIN","CC-EE","FINAL",1,"Y");
     }
