0b015d4b283a3840c452259364b5d62ef8ea6186
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-05 13:44:39 -0400

    new fees

---
+++ Scripts/Event/WTUA;ENVENGINEERING!~!~!~.js
@@ -0,0 +1,11 @@
+//Fees
+var TotalLDAcreage = parseFloat(AInfo['Total Land Disturbance Acreage']);
+if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEngineering/Plan Review/ESC Plan/NA')) && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage <=.229)) {
+    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+    }
+if ((appMatch('EnvEngineering/Plan Review/Linear Project/NA') || appMatch('EnvEngineering/Plan Review/ESC Plan/NA')) && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (TotalLDAcreage >.229)) {
+    addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+    }
+    if (appMatch('EnvEngineering/Plan Review/Timbering/NA') && (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment')) {
+        addFee("ERSCRENFMIN","CC-PLANNING","FINAL",1,"N");
+        }
