c5867dadedebe5875b402210532f4bd0fa01d428
Ray Schug <RSchug@TruePointSolutions.com>
2020-08-06 15:24:44 -0700

    24P Deferral Request Fee

--- INCLUDES_CUSTOM/getWorkflowHistory_TPS.js
+++ INCLUDES_CUSTOM/getWorkflowHistory_TPS.js
@@ -5,7 +5,7 @@ function getWorkflowHistory_TPS() { // Get Workflow History.
     var itemCap = arguments.length > 3 && arguments[3] ? arguments[3] : capId;
 
     var useProcess = processName != "" ? true : false;
-    var wfStatArray = wfstat && typeof (wfstat) == "string" ? [wfstat] : wfstat; // Convert to array
+    var wfStatArray = wfstat && typeof (wfstat) == "string" ? [wfstat] : wfStat; // Convert to array
     if (typeof (itemCap) == "string") {
         var result = aa.cap.getCapID(itemCap);
         if (result.getSuccess()) {
@@ -17,7 +17,7 @@ function getWorkflowHistory_TPS() { // Get Workflow History.
 
     var taskArr = new Array();
 
-    var workflowResult = aa.workflow.getHistory(itemCap).getOutput();
+    var workflowResult = aa.workflow.getHistory(itemCap);
     if (!workflowResult.getSuccess()) {
         logDebug("**ERROR: Failed to get workflow object: " + workflowResult.getErrorMessage());
         return false;
@@ -31,6 +31,13 @@ function getWorkflowHistory_TPS() { // Get Workflow History.
         if (wfstr && !fTask.getTaskDescription().equals(wfstr)) continue;
         if (wfStatArray && !exists(wfTask, fTask.getDisposition(), wfStatArray)) continue;
         tasks.push(fTask);
+        logDebug("History[" + x + "]: # " + fTask.getProcessHistorySeq()
+            + ", ID: " + fTask.getProcessID() + "." + fTask.getStepNumber()
+            + " , TaskID:" + fTask.getCurrentTaskID() + " - " + fTask.getNextTaskID()
+            + ", Task: " + fTask.getProcessCode() + "." + fTask.getTaskDescription()
+            + ", Status: " + fTask.getDisposition() + " " + fTask.getStatusDate()
+            //  + br + describe_TPS(fTask)
+        );
     }
     return tasks;
 }
