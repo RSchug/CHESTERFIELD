1dcd094be1d06080dd4306a1f8f640d7f09d6ea8
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-06 11:56:40 -0400

    test

--- Scripts/Event/PRA;PLANNING!~!~!~.js
+++ Scripts/Event/PRA;PLANNING!~!~!~.js
@@ -1,10 +1,9 @@
 //3P. Fee Balance = 0 THEN: closeTask = 'Fee Payment' and activate ad hocs (per the record types)
 if (balanceDue = 0) {
     closeTask("Fee Payment","Payment Received","Updated based on Balance of 0","");
-    addAdHocTask("ADHOC_WORKFLOW","Public Notices","");
-    addAdHocTask("ADHOC_WORKFLOW","Adjacents","");
-    addAdHocTask("ADHOC_WORKFLOW","IVR Message","");
-    addAdHocTask("ADHOC_WORKFLOW","Sign Posting","");
-    addAdHocTask("ADHOC_WORKFLOW","Maps","");
-	
+//    addAdHocTask("ADHOC_WORKFLOW","Public Notices","");
+//    addAdHocTask("ADHOC_WORKFLOW","Adjacents","");
+//    addAdHocTask("ADHOC_WORKFLOW","IVR Message","");
+//    addAdHocTask("ADHOC_WORKFLOW","Sign Posting","");
+//    addAdHocTask("ADHOC_WORKFLOW","Maps","");
 }
