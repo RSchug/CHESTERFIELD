2cc919288d9322736ad155c0fa77d2b1f5918092
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-06 12:30:49 -0400

    new variable

--- Scripts/Event/PRA;PLANNING!~!~!~.js
+++ Scripts/Event/PRA;PLANNING!~!~!~.js
@@ -1,5 +1,27 @@
 //3P. Fee Balance = 0 THEN: closeTask = 'Fee Payment' and activate ad hocs (per the record types)
-if (isTaskActive("Fee Payment") && (balanceDue = 0)) {
+if balanceDue = 0 {
+    var workflowResult = aa.workflow.getTasks(capId);
+if (workflowResult.getSuccess())
+    var wfObj = workflowResult.getOutput();
+else
+     {
+     logMessage("**ERROR","CAP # "+capId.getCustomID()+" Failed to get workflow object: " + workflowResult.getErrorMessage());
+     return false;
+     }
+var fTask;
+var stepnumber;
+var processID;
+var dispositionDate = aa.date.getCurrentDate();
+var wfnote = " ";
+var wftask;
+for (i in wfObj)
+     {
+     fTask = wfObj[i];
+     wftask = fTask.getTaskDescription();
+     stepnumber = fTask.getStepNumber();
+     processID = fTask.getProcessID();
+     if (fTask.getActiveFlag().equals("Y") && fTask.getTaskDescription('Fee Payment'))
+        {
     closeTask("Fee Payment","Payment Received","Updated based on Balance of 0","");
 //    addAdHocTask("ADHOC_WORKFLOW","Public Notices","");
 //    addAdHocTask("ADHOC_WORKFLOW","Adjacents","");
@@ -7,3 +29,4 @@ if (isTaskActive("Fee Payment") && (balanceDue = 0)) {
 //    addAdHocTask("ADHOC_WORKFLOW","Sign Posting","");
 //    addAdHocTask("ADHOC_WORKFLOW","Maps","");
 }
+     }}
