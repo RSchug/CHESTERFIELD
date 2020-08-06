24120a251dcf2336dd358a4eee40db2242ad5ada
Ray Schug <RSchug@TruePointSolutions.com>
2020-08-06 15:02:20 -0700

    Fix wasTaskStatus_TPS

--- INCLUDES_CUSTOM/wasTaskStatus_TPS.js
+++ INCLUDES_CUSTOM/wasTaskStatus_TPS.js
@@ -1,11 +1,19 @@
 function wasTaskStatus_TPS(wfstr, wfstat) {
-	var useProcess = arguments.length > 2 && arguments[2]? true:false;
-	var itemCap = arguments.length > 3 && arguments[3]? arguments[3]:capId;
-	var processName = useProcess? arguments[2]:"";
-	var wfStatArray = (wfStat && typeof(wfStat) == "string"? [wfStat]:wfStat); // Convert to array
- 
-	
-	var workflowResult = aa.workflow.getHistory(itemCap).getOutput();
+	var processName = arguments.length > 2 && arguments[2] ? arguments[2] : "";
+	var itemCap = arguments.length > 3 && arguments[3] ? arguments[3] : capId;
+
+	var useProcess = processName != "" ? true : false;
+	var wfStatArray = wfstat && typeof (wfstat) == "string" ? [wfstat] : wfStat; // Convert to array
+	if (typeof (itemCap) == "string") {
+		var result = aa.cap.getCapID(itemCap);
+		if (result.getSuccess()) {
+			itemCap = result.getOutput();
+		} else {
+			logMessage("**ERROR: Failed to get cap ID: " + result + " error: " + result.getErrorMessage()); return false;
+		}
+	}
+
+	var workflowResult = aa.workflow.getHistory(itemCap);
 	if (!workflowResult.getSuccess()) {
 		logDebug("**ERROR: Failed to get workflow object: " + workflowResult.getErrorMessage());
 		return false;
@@ -13,9 +21,10 @@ function wasTaskStatus_TPS(wfstr, wfstat) {
 
 	var wfObj = workflowResult.getOutput();
 	for (var x = 0; x < wfObj.length; x++) {
+		var fTask = wfObj[x];
 		if (useProcess && !fTask.getProcessCode().equals(processName)) continue;
-		if (wfstr && wfstr != wfObj[x].getTaskDescription()) continue;
-		if (wfStatArray && !exists(wfTask,wfObj[x].getTaskDescription(),wfStatArray)) continue;
+		if (wfstr && !fTask.getTaskDescription().equals(wfstr)) continue;
+		if (wfStatArray && !exists(wfTask, fTask.getDisposition(), wfStatArray)) continue;
 		return true;
 	}
 	return false;
