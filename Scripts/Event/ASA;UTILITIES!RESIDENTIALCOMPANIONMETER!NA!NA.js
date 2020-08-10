c7225e2e1dfaf532e299d9510f9a0d9727831b95
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-10 14:02:49 -0400

    NEW

---
+++ Scripts/Event/ASA;UTILITIES!RESIDENTIALCOMPANIONMETER!NA!NA.js
@@ -0,0 +1,5 @@
+var newTapNumber = Number(lookup("NEW_TAP_NUMBER","TAP_NUMBER"));
+var count = 1;
+editAppSpecific("TAP Number",lookup("NEW_TAP_NUMBER","TAP_NUMBER"));
+editLookup("NEW_TAP_NUMBER","TAP_NUMBER",zeroPad(newTapNumber+count,8));
+}
