7fce47a9dae814fbcdd5cb1955ac490c94703f0a
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-11 08:09:00 -0400

    amendment

--- Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!NA.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!NA.js
@@ -42,3 +42,27 @@
 //    closeTask("Permit Issuance","Issued","Issued as MultiUnit","")
 //    capId = saveCapId;
 //    }
+if (wfStatus == 'Amendment Submitted') {
+    var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
+    if (appMatch("Building/Permit/Residential/NA"))
+        var newAppTypeString = "Building/Permit/Residential/Amendment";
+    var newCapName = capName;
+    var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
+    var newCapRelation = "Child";
+    var srcCapId = capId;
+    var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
+    if (newCapId) {
+        showMessage = true;
+        comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
+            + "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
+        if (wfComment && wfComment != "") {
+            cWorkDesc = workDescGet(capId);
+            nWorkDesc = cWorkDesc + ", " + wfComment;
+            updateWorkDesc(nWorkDesc, newCapId);
+        }
+        // set the Permit Expiration Date to 180 days from Application Date.
+        var expDateField = "Permit Expiration Date";
+        var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
+        editAppSpecific(expDateField, expDate, newCapId);
+    }
+}
