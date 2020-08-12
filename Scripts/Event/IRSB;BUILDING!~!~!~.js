b0c2bbdef185389819fec9d17ddff2ca7e2b5e56
bushatos <66446858+bushatos@users.noreply.github.com>
2020-08-11 22:15:06 -0400

    Update per MB update

---
+++ Scripts/Event/IRSB;BUILDING!~!~!~.js
@@ -0,0 +1,17 @@
+try {
+// Error message when Fee item selected with wrong status
+	if (inspResult == "Approved") {  //&& Overtime == 'CHECKED'
+			showMessage = true;
+			comment('<font size=small><b>Not Ready Fee must be Corrections Required status</b></font>');
+			// if (!exists(vEventName, ["InspectionMultipleScheduleAfter", "InspectionMultipleScheduleBefore"])) 
+			cancel = true;
+	}
+	/*if (inspResult == "Cancelled" && (Overtime == 'CHECKED' || Billable == 'CHECKED')) {
+			showMessage = true;
+			comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
+			// if (!exists(vEventName, ["InspectionMultipleScheduleAfter", "InspectionMultipleScheduleBefore"])) 
+			cancel = true;
+	}*/
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
