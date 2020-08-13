89bda16fef0ff223b02eaf8b3c854402b2dace6d
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-13 13:25:49 -0400

    removed as does not work

--- Scripts/Event/IRSB;BUILDING!~!~!~.js
+++ Scripts/Event/IRSB;BUILDING!~!~!~.js
@@ -1,24 +1,25 @@
-try {
-	var inspBillable = inspObj.getInspection().getActivity().getInspBillable();
-	logDebug("Inspection Billable checkbox = " + inspBillable + ". And Inspection Result = " + inspResult);
-	var inspOvertime = inspObj.getInspection().getActivity().getOvertime();
-	logDebug("Inspection Not Ready (Overtime) checkbox = " + inspOvertime + ". And Inspection Result = " + inspResult);
+//does not work as variable are only assigned after not before
+//try {
+//	var inspBillable = inspObj.getInspection().getActivity().getInspBillable();
+//	logDebug("Inspection Billable checkbox = " + inspBillable + ". And Inspection Result = " + inspResult);
+//	var inspOvertime = inspObj.getInspection().getActivity().getOvertime();
+//	logDebug("Inspection Not Ready (Overtime) checkbox = " + inspOvertime + ". And Inspection Result = " + inspResult);
 // Error message when Fee item selected with wrong status
-if (inspResult == "Approved" && inspOvertime == "Y") {
-			showMessage = true;
-			comment('<font size=small><b>Not Ready Fee must be Corrections Required status</b></font>');
-			cancel = true;
-	}
-	if (inspResult == "Cancelled" && inspBillable =="Y") {
-			showMessage = true;
-			comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
-			cancel = true;
-	}
-	if (inspResult == "Cancelled" && inspOvertime =="Y") {
-		showMessage = true;
-		comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
-		cancel = true;
-}
-} catch (err) {
-	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
-}
+//if (inspResult == "Approved" && inspOvertime == "Y") {
+//			showMessage = true;
+//			comment('<font size=small><b>Not Ready Fee must be Corrections Required status</b></font>');
+//			cancel = true;
+//	}
+//	if (inspResult == "Cancelled" && inspBillable =="Y") {
+//			showMessage = true;
+//			comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
+//			cancel = true;
+//	}
+//	if (inspResult == "Cancelled" && inspOvertime =="Y") {
+//		showMessage = true;
+//		comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
+//		cancel = true;
+//}
+//} catch (err) {
+//	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+//}
