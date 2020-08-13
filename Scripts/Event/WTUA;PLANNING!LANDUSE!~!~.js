31bd02e5654015c21b9eb6bfe8c8aa36159412e6
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-12 21:31:31 -0400

    48p

--- Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
@@ -4,3 +4,9 @@ if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Move to BOS")
 	activateTask("BOS Staff Report")
 	deactivateTask("Review Consolidation")
 	}
+//48P
+	if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Move to CPC"))){
+	activateTask("CPC Hearing");
+	activateTask("CPC Staff Report")
+	deactivateTask("Review Consolidation")
+	}
