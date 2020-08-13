88533ad195651226df3a662541f75bf471f4473c
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-12 20:28:41 -0400

    fix

--- Scripts/Event/IRSB;BUILDING!~!~!~.js
+++ Scripts/Event/IRSB;BUILDING!~!~!~.js
@@ -1,8 +1,7 @@
-try {
 	var inspBillable = inspObj.getInspection().getActivity().getInspBillable();
 	var inspOvertime = inspObj.getInspection().getActivity().getOvertime();
 // Error message when Fee item selected with wrong status
-if (inspResult == "Approved") && inspOvertime == "Y"{
+if (inspResult == "Approved" && inspOvertime == "Y") {
 			showMessage = true;
 			comment('<font size=small><b>Not Ready Fee must be Corrections Required status</b></font>');
 			cancel = true;
@@ -12,6 +11,3 @@ if (inspResult == "Approved") && inspOvertime == "Y"{
 			comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
 			cancel = true;
 	}
-} catch (err) {
-    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
-}
