253874b25e3b862b0f5f034068cf75bba616e12d
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-12 21:28:09 -0400

    update

--- Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
@@ -1,5 +1,6 @@
 //6P
 if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Move to BOS"))){
 	activateTask("BOS Hearing");
+	activateTask("BOS Staff Report")
 	deactivateTask("Review Consolidation")
-		}
+	}
