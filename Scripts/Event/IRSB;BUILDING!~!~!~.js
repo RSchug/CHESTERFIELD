2187046378203e319c655e3333c67c7864c258f7
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-12 20:27:28 -0400

    add variable

--- Scripts/Event/IRSB;BUILDING!~!~!~.js
+++ Scripts/Event/IRSB;BUILDING!~!~!~.js
@@ -1,15 +1,17 @@
 try {
+	var inspBillable = inspObj.getInspection().getActivity().getInspBillable();
+	var inspOvertime = inspObj.getInspection().getActivity().getOvertime();
 // Error message when Fee item selected with wrong status
-/*	if (inspResult == "Approved") {  //&& Overtime == 'CHECKED'
+if (inspResult == "Approved") && inspOvertime == "Y"{
 			showMessage = true;
 			comment('<font size=small><b>Not Ready Fee must be Corrections Required status</b></font>');
 			cancel = true;
 	}
-	if (inspResult == "Cancelled" && (Overtime == 'CHECKED' || Billable == 'CHECKED')) {
+	if (inspResult == "Cancelled" && (inspBillable =="Y" || inspOvertime=="Y")) {
 			showMessage = true;
 			comment('<font size=small><b>Fees are not allowed for Cancelled Status</b></font>');
 			cancel = true;
-	}*/
+	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
