19f911c98997ee9c1654cf5b88583effe18c58c7
Ray Schug <RSchug@TruePointSolutions.com>
2020-08-19 09:28:03 -0700

    32CE: Abatement Fee Exists

---
+++ Scripts/Event/WTUB;ENFORCEMENT!~!~!~.js
@@ -0,0 +1,9 @@
+// WTUB:Enforcement/*/*/*
+// 32CE: When Workflow Task Status = Invoice Issued
+//      THEN: Check to make sure an Abatement Fee is added on the record and if not abatement fee is on the record
+//      THEN: Display message "No Abatement Fee Assessed"
+if (wfStatus == 'Invoice Issued' && feeExists("CC-ENF-ZON", "NEW","INVOICED")) {
+    showMessage = true;
+    comment('No Abatement Fee Assessed');
+    cancel = true;
+}
