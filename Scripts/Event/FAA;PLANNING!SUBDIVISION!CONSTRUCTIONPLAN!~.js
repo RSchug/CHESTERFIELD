a280d587fe2ba272d8f638a4eebf40ecee8b2229
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-24 14:20:21 -0400

    remove

--- Scripts/Event/FAA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
+++ Scripts/Event/FAA;PLANNING!SUBDIVISION!CONSTRUCTIONPLAN!~.js
@@ -1,3 +1,4 @@
-if (feeExists("CONSTPLAN4") && !feeExists("EECPADJUST")) {
-    addFee('EECPADJUST','CC-PLANNING','FINAL',1,'Y');
-}
+//County decided to change on 8/21/2020
+//if (feeExists("CONSTPLAN4") && !feeExists("EECPADJUST")) {
+//    addFee('EECPADJUST','CC-PLANNING','FINAL',1,'Y');
+//}
