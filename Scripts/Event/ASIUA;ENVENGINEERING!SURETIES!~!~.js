c20fe1bf583671a31eeccd6cc1f51d27edb452fa
Ray Schug <RSchug@TruePointSolutions.com>
2020-08-27 07:28:17 -0700

    153EE Sureties Pending Release

--- Scripts/Event/ASIUA;ENVENGINEERING!SURETIES!~!~.js
+++ Scripts/Event/ASIUA;ENVENGINEERING!SURETIES!~!~.js
@@ -3,6 +3,7 @@
 |        THEN: Check and see if ALL SURETIES custom lists rows column = Surety Status has a status of "Released" or "Replaced"; if they do
 |        THEN: Update the Sureties Workflow Task with a Status of Pending Release; deactivate the Sureties Workflow Task; activate the Final Processing Workflow Task; update the Final Processing Workflow Task with a Workflow Task Status = "Pending Release Review"
 */
+if (isTaskActive("Sureties")) {
 	var tableName = "SURETIES";
 	logDebug("Loading " + tableName);
 	var	wfUpdate = true;
@@ -22,4 +23,4 @@
         resultWorkflowTask(wfTask, wfStatus, "Updated via script", "");
         activateTask(wfTask)
     }
-
+}
