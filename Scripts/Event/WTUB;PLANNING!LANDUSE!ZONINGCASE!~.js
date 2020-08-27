21c9c60a140dcd240fa03842758581cc332560bf
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-27 15:57:29 -0400

    fix

--- Scripts/Event/WTUB;PLANNING!LANDUSE!ZONINGCASE!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!ZONINGCASE!~.js
@@ -10,7 +10,7 @@ try {
         }
     if (matches(wfTask, 'BOS Hearing') && matches(wfStatus,'Approved','Denied')) {
 		if (AInfo['BOS Conditions'] == null || AInfo['BOS Proffered Conditions'] == null || AInfo['BOS Cash Proffers'] == null || AInfo['BOS Complies with Plan'] == null || AInfo['BOS Approved Time Limit'] == null || AInfo['BOS Expiration Date'] == null
-		|| AInfo['BOS Residential - Single Family Unit Approved'] == null || AInfo['BOS Residential - Multi Family Unit Approved '] == null || AInfo['BOS Age Restricted Units'] == null) {
+		|| AInfo['BOS Residential - Single Family Unit Approved'] == null || AInfo['BOS Residential - Mulit Family Unit Approved'] == null || AInfo['BOS Age Restricted Units'] == null) {
 			showMessage = true;
 			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 			cancel = true;
