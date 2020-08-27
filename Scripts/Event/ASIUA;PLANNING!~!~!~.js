08383b91c0d6466adff13aee8101f981a4295561
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-26 21:15:36 -0400

    parseFloat

--- Scripts/Event/ASIUA;PLANNING!~!~!~.js
+++ Scripts/Event/ASIUA;PLANNING!~!~!~.js
@@ -13,10 +13,10 @@ if (tempAsit) {
     var countParcels = 0;
 	for (a in tempAsit) {
         if (!isNaN(tempAsit[a]["Parcel Acreage"])) {
-            parcelAcreage += parseFloat(tempAsit[a]["Parcel Acreage"]).toFixed(2);
+            parcelAcreage += parseFloat((tempAsit[a]["Parcel Acreage"]);
         }
         if (!isNaN(tempAsit[a]["Revised Acreage"])) {
-            revisedAcreage += parseFloat(tempAsit[a]["Revised Acreage"]).toFixed(2);
+            revisedAcreage += parseFloat(tempAsit[a]["Revised Acreage"]);
         }
         countParcels++;
 	}//for all rows
