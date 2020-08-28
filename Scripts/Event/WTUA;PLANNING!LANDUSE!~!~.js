e24604685b24c15ff68e0409e04fd4f59b1f65b0
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-28 09:10:11 -0400

    fix

--- Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!~!~.js
@@ -1,14 +1,14 @@
 //6P
 if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Move to BOS"))){
 	activateTask("BOS Hearing");
-	activateTask("BOS Staff Report")
-	deactivateTask("Review Consolidation")
+	activateTask("BOS Staff Report");
+	deactivateTask("Review Consolidation");
 	}
 //48P
 	if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Move to CPC"))){
 	activateTask("CPC Hearing");
-	activateTask("CPC Staff Report")
-	deactivateTask("Review Consolidation")
+	activateTask("CPC Staff Report");
+	deactivateTask("Review Consolidation");
 	}
 //90P
 if ((matches(wfTask,"Review Consolidation")) && (matches(wfStatus,"Ready for BZA"))){
