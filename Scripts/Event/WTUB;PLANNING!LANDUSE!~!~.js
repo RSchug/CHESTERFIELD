b638a9c5c15a89e2d6781dfc0af7467a1399b653
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-08-29 13:16:56 -0400

    42.4

--- Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
@@ -8,6 +8,18 @@ if (appMatch('Planning/LandUse/AdminVariance/NA','Planning/LandUse/Variance/NA',
 			cancel = true;
 		}
 	}
-}} catch (err) {
+}
+// 42.4P Manufactured Homes and RPA Exception
+if (appMatch('Planning/LandUse/ManufacturedHomes/NA','Planning/LandUse/RPAException/NA')){    
+    if (matches(wfTask, 'BOS Hearing') && matches(wfStatus,'Approved','Denied')) {
+		if (AInfo['BOS Conditions'] == null || AInfo['BOS Proffered conditions'] == null || AInfo['BOS Cash proffers'] == null || AInfo['BOS Complies with plan'] == null || AInfo['BOS Approved time limit'] == null || AInfo['BOS Expiration date'] == null) {
+			showMessage = true;
+			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
+			cancel = true;
+		}
+	}
+} 
+}
+catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
