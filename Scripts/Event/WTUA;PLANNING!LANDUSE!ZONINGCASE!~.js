0e53c7ed3d1eee6d4d16bbd08ab54c1c51c9083e
bushatos <66446858+bushatos@users.noreply.github.com>
2020-08-31 14:48:22 -0400

    Updates for 60p

--- Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!ZONINGCASE!~.js
@@ -17,6 +17,49 @@ try {
 			}//for all rows
 		}
 	}
+	 // Set the Reviewers Tasks per the wfStatus choosen per REVIEW DEPTS FOR ELM Spreadsheet scritp# 60p
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Commercial Review') {
+		activateTask("CDOT Review");
+		activateTask("Community Enhancement Review");
+		activateTask("Environmental Engineering Review");
+		activateTask("Fire and Life Safety Review");
+		activateTask("Planning Review");
+		activateTask("Utilities Review");
+		activateTask("VDOT Review");
+	}
+	if (wfTask == 'Review Distribution' && (wfStatus == 'Routed for Residential and Commercial' || wfStatus == 'Routed for Residential Review')) {
+		activateTask("Budget Review");
+		activateTask("CDOT Review");
+		activateTask("Community Enhancement Review");
+		activateTask("Department of Health Review");
+		activateTask("Environmental Engineering Review");
+		activateTask("Fire and Life Safety Review");
+		activateTask("County Library Review");
+		activateTask("Parks and Recreation Review");
+		activateTask("Planning Review");
+		activateTask("Utilities Review");
+		activateTask("VDOT Review");
+		activateTask("School Research and Planning Review");
+		if (wfStatus == 'Routed for Residential and Commercial') {
+			activateTask("General Services");
+		}
+	}
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Towers Review') {
+		activateTask("Airport Review");
+		activateTask("CDOT Review");
+		activateTask("Community Enhancement Review");
+		activateTask("Environmental Engineering Review");
+		activateTask("Fire and Life Safety Review");
+		activateTask("Planning Review");
+		activateTask("Utilities Review");
+		activateTask("VDOT Review");
+		activateTask("General Services");
+		activateTask("Radio Shop");
+	}
+	if (wfTask == 'Review Consolidation' && wfStatus == 'Move to BOS') {
+		activateTask("BOS Staff Report");
+		deactivateTask("Review Consolidation");
+	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
--- Scripts/Event/WTUA;PLANNING!SITEPLAN!MINOR!~.js
+++ Scripts/Event/WTUA;PLANNING!SITEPLAN!MINOR!~.js
@@ -1,32 +1,52 @@
+try {
+    // Set the Reviewers Tasks per the wfStatus choosen per REVIEW DEPTS FOR ELM Spreadsheet scritp# 60p
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Review') {
+		activateTask("Building Inspection Review");
+		activateTask("CDOT Review");
+		activateTask("Environmental Engineering Review");
+		activateTask("Fire and Life Safety Review");
+		activateTask("Planning Review");
+		activateTask("Police Review");
+		activateTask("Utilities Review");
+		activateTask("VDOT Review");
+		activateTask("Real Property Review");
+		activateTask("GIS-IST Review");
+		activateTask("Water Quality Review");
+	}
+	
 //33P set the Expiration Date to 1825 days 
-var expDateField = "Expiration Date";
-var expDate = jsDateToASIDate(new Date(dateAdd(null, 1825)));
-if (wfTask == 'Review Consolidation' && wfStatus == 'Approved'){
-    editAppSpecific(expDateField, expDate);
-}
+	var expDateField = "Expiration Date";
+	var expDate = jsDateToASIDate(new Date(dateAdd(null, 1825)));
+	if (wfTask == 'Review Consolidation' && wfStatus == 'Approved'){
+		editAppSpecific(expDateField, expDate);
+	}
 //80p
-if (wfTask == 'Staff and Developer Meeting' && wfStatus == 'Complete'){
-    setTask("Planning Review","N","Y");
-    setTask("Airport Review","N","Y");
-    setTask("Assessor Review","N","Y");
-    setTask("Building Inspection Review","N","Y");
-    setTask("County Library Review","N","Y");
-    setTask("Department of Health Review","N","Y");
-    setTask("CDOT Review","N","Y");
-    setTask("Economic Development Review","N","Y");
-    setTask("Environmental Engineering Review","N","Y");
-    setTask("Fire and Life Safety Review","N","Y");
-    setTask("GIS-IST Review","N","Y");
-    setTask("GIS-EDM Utilities Review","N","Y");
-    setTask("Parks and Recreation Review","N","Y");
-    setTask("Police Review","N","Y");
-    setTask("Real Property Review","N","Y");
-    setTask("School Board Review","N","Y");
-    setTask("School Research and Planning Review","N","Y");
-    setTask("Utilities Review","N","Y");
-    setTask("VDOT Review","N","Y");
-    setTask("Water Quality Review","N","Y");
-    setTask("Chesterfield Historical Society Review","N","Y");
-    setTask("Community Enhancement Review","N","Y");
-    activateTask("Review Consolidation");
+	if (wfTask == 'Staff and Developer Meeting' && wfStatus == 'Complete'){
+		setTask("Planning Review","N","Y");
+		setTask("Airport Review","N","Y");
+		setTask("Assessor Review","N","Y");
+		setTask("Building Inspection Review","N","Y");
+		setTask("County Library Review","N","Y");
+		setTask("Department of Health Review","N","Y");
+		setTask("CDOT Review","N","Y");
+		setTask("Economic Development Review","N","Y");
+		setTask("Environmental Engineering Review","N","Y");
+		setTask("Fire and Life Safety Review","N","Y");
+		setTask("GIS-IST Review","N","Y");
+		setTask("GIS-EDM Utilities Review","N","Y");
+		setTask("Parks and Recreation Review","N","Y");
+		setTask("Police Review","N","Y");
+		setTask("Real Property Review","N","Y");
+		setTask("County Attorney Review","N","Y");
+		setTask("School Research and Planning Review","N","Y");
+		setTask("Utilities Review","N","Y");
+		setTask("VDOT Review","N","Y");
+		setTask("Water Quality Review","N","Y");
+		setTask("Chesterfield Historical Society Review","N","Y");
+		setTask("Community Enhancement Review","N","Y");
+		activateTask("Review Consolidation");
+	}
+	
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
