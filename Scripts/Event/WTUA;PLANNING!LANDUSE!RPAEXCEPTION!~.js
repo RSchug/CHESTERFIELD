5ba13083e14d9d8ea3f5e44fd6cc816315179c4f
bushatos <66446858+bushatos@users.noreply.github.com>
2020-08-30 23:54:54 -0400

    Added scripts for 60p

---
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!RPAEXCEPTION!~.js
@@ -0,0 +1,13 @@
+try {
+    // Set the Reviewers Tasks per the wfStatus choosen per REVIEW DEPTS FOR ELM Spreadsheet scritp# 60p
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Review') {
+		activateTask("Environmental Engineering Review");
+		activateTask("Parks and Recreation Review");
+		activateTask("Planning Review");
+		activateTask("Utilities Review");
+		activateTask("Real Property Review");
+		activateTask("Water Quality Review");
+	}
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
