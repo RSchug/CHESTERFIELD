5bf2920708a55fb09d8c80a35ea06520d93f586a
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-03 19:49:51 -0400

    new names

--- Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
@@ -1,18 +1,8 @@
 try {
 	// CPC Hearing Results required for Subdivisions
-	if (appMatch('*/*/ConstructionPlan/NA')|| appMatch('*/*/OverallConceptualPlan/NA')|| appMatch('*/*/Preliminary/NA')){
+	if (appMatch('*/*/ConstructionPlan/NA')|| appMatch('*/*/OverallConceptualPlan/NA')|| appMatch('*/*/Preliminary/NA') || appMatch('*/*/ExceptiontoPreliminary/NA')){
 		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
 				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
-					showMessage = true;
-					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
-					cancel = true;
-				}
-			}
-	}
-	// CPC Hearing Results required for Subdivision Exception to Preliminary
-	if (appMatch('*/*/ExceptiontoPreliminary/NA')){
-		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
-				if (AInfo['Conditions'] == null || AInfo['Approved time limit'] == null || AInfo['Expiration Date'] == null) {
 					showMessage = true;
 					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 					cancel = true;
