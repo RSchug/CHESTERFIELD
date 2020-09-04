c87ebaa76f6eacf04d59bace445a7b00f12ff42a
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-03 21:10:15 -0400

    fix

--- Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
@@ -12,7 +12,7 @@ if (appMatch('Planning/LandUse/AdminVariance/NA') || appMatch('Planning/LandUse/
 // 42.4P Manufactured Homes and RPA Exception
 if (appMatch('Planning/LandUse/ManufacturedHomes/NA') || appMatch('Planning/LandUse/RPAException/NA')){    
     if (matches(wfTask, 'BOS Hearing') && matches(wfStatus,'Approved','Denied')) {
-		if (AInfo['BOS Conditions'] == null || AInfo['BOS Proffered conditions'] == null || AInfo['BOS Cash proffers'] == null || AInfo['BOS Complies with plan'] == null || AInfo['BOS Approved time limit'] == null || AInfo['BOS Expiration date'] == null) {
+		if (AInfo['Conditions'] == null || AInfo['Proffered Conditions'] == null || AInfo['Cash Proffers'] == null || AInfo['Complies with Plan'] == null || AInfo['BOS Approved time limit'] == null || AInfo['BOS Expiration date'] == null) {
 			showMessage = true;
 			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 			cancel = true;
