cb74b142057ae10177390aa846ca22bff5ceebc0
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-03 20:55:23 -0400

    92p

--- Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
@@ -18,7 +18,16 @@ if (appMatch('Planning/LandUse/ManufacturedHomes/NA') || appMatch('Planning/Land
 			cancel = true;
 		}
 	}
-} 
+}
+if (appMatch('Planning/LandUse/AdminVariance/NA')){    
+    if (matches(wfTask, 'Administrative Outcome') && matches(wfStatus,'Approved','Denied')) {
+		if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
+			showMessage = true;
+			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
+			cancel = true;
+		}
+	}
+}
 }
 catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
