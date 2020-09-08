c123cf52e69ac843d30996e560cf49f7ebc00708
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-08 10:00:18 -0400

    31B update

--- Scripts/Event/IRSA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/IRSA;BUILDING!PERMIT!~!~.js
@@ -72,9 +72,9 @@ if (inspType.equals("Building Final") && inspResult.equals("Approved")
 	}
 }
 //31B update for Residential Fire to close out Inspections Workflow
-if (appMatch("Building/Permit/Residential/Fire") && (matches(inspType,"Final Alarm","Final Clean Agent","Final Hood Suppression","Final Pump Test","Final Sprinkler","Final Standpipe System","Final Underground Inspection")) && inspResult.equals("Approved")){
-	closeTask("Inspections","Completed","Updated based on Completed Inspection Result","");
+if (appMatch("Building/Permit/Residential/Fire") && (matches(inspType,"Spk Final Inspection","UGD Final Inspection")) && inspResult.equals("Approved")){
+	closeTask("Inspections","Completed","Updated based on Approved Inspection Result","");
 }
-if (appMatch("Building/Permit/Commercial/Fire") && (matches(inspType,"Final Alarm","Final Clean Agent","Final Hood Suppression","Final Pump Test","Final Sprinkler","Final Standpipe System","Final Underground Inspection")) && inspResult.equals("Approved")){
-	closeTask("Inspections","Completed","Updated based on Completed Inspection Result","");
+if (appMatch("Building/Permit/Commercial/Fire") && (matches(inspType,"Alm Final Inspection","Cln Agt Final Inspection","Hood Suppression Final","Spk Final Inspection","UGD Final Inspection")) && inspResult.equals("Approved")){
+	closeTask("Inspections","Completed","Updated based on Approved Inspection Result","");
 }
