300fa8fd4a6311e30d735c430a9c84a662cd2165
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-10 19:30:41 -0400

    93p

--- Scripts/Event/WTUB;PLANNING!SITEPLAN!MINOR!~.js
+++ Scripts/Event/WTUB;PLANNING!SITEPLAN!MINOR!~.js
@@ -14,14 +14,14 @@ try {
         }
     }
 // Results required 93P
-if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Approval')) {
-    if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Non-Residential Gross Building Square Feet'] == null
-    || AInfo['Expiration Date'] == null || AInfo['Number of Single Family Units Approved'] == null || AInfo['Number of Multi Family Units Approved'] == null || AInfo['Total Pedestrian Paths'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Pedestrian Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Sidewalk Width'] == null || AInfo['Sidewalks'] == null) {
-        showMessage = true;
-        comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
-        cancel = true;
-    }
-}
+//if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Approval')) {
+//    if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Non-Residential Gross Building Square Feet'] == null
+//    || AInfo['Expiration Date'] == null || AInfo['Number of Single Family Units Approved'] == null || AInfo['Number of Multi Family Units Approved'] == null || AInfo['Total Pedestrian Paths'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Pedestrian Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Sidewalk Width'] == null || AInfo['Sidewalks'] == null) {
+//        showMessage = true;
+//        comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
+//        cancel = true;
+//    }
+//}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
