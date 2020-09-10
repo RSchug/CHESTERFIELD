ac7b70acf9f32becc15f87fcd25509b894cc5819
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-10 19:46:40 -0400

    93P

---
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!EXCEPTIONTOPRELIMINARY!~.js
@@ -0,0 +1,8 @@
+// Results required 93P
+if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Approval')) {
+    if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Expiration Date'] == null) {
+        showMessage = true;
+        comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
+        cancel = true;
+    }
+}
