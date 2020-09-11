fa68f099f8196fbea892b5f1457ca79e66587a07
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-11 16:59:02 -0400

    remove exp date

--- Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
@@ -2,7 +2,7 @@ try {
 	// CPC Hearing Results required for Subdivisions
 	if (appMatch('*/*/ConstructionPlan/NA')|| appMatch('*/*/OverallConceptualPlan/NA')|| appMatch('*/*/ExceptiontoPreliminary/NA')){
 		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
-				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
+				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null) {
 					showMessage = true;
 					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 					cancel = true;
@@ -11,7 +11,7 @@ try {
 	}
 	if (appMatch('*/*/Preliminary/NA')){
 		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
-				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null || AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null){
+				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null){
 					showMessage = true;
 					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 					cancel = true;
