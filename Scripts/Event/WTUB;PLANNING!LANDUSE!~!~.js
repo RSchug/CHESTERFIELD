5785219c014737c2c8d3e95ba0ebb15d144720c7
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-11 17:01:57 -0400

    remove exp date

--- Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
+++ Scripts/Event/WTUB;PLANNING!LANDUSE!~!~.js
@@ -2,7 +2,7 @@ try {
 // Variances BZA
 if (appMatch('Planning/LandUse/AdminVariance/NA') || appMatch('Planning/LandUse/Variance/NA') || appMatch('Planning/LandUse/SpecialException/NA') || appMatch('Planning/LandUse/Appeal/NA')){    
     if (matches(wfTask, 'BZA Hearing') && matches(wfStatus,'Approved','Denied')) {
-		if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
+		if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null) {
 			showMessage = true;
 			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 			cancel = true;
@@ -22,7 +22,7 @@ if (appMatch('Planning/LandUse/ManufacturedHomes/NA') || appMatch('Planning/Land
 if (appMatch('Planning/LandUse/ManufacturedHomes/NA') || appMatch('Planning/LandUse/RPAException/NA')){   
 if (matches(wfTask, 'BOS Hearing') && matches(wfStatus, 'Approved','Denied')) {
 	if (AInfo['No BOS Time Limit'] != 'CHECKED'){
-		if (AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
+		if (AInfo['Approved Time Limit'] == null) {
 		showMessage = true;
 		comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 		cancel = true;
@@ -32,7 +32,7 @@ if (matches(wfTask, 'BOS Hearing') && matches(wfStatus, 'Approved','Denied')) {
 }
 if (appMatch('Planning/LandUse/AdminVariance/NA')){    
     if (matches(wfTask, 'Administrative Outcome') && matches(wfStatus,'Approved','Denied')) {
-		if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Expiration Date'] == null) {
+		if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null) {
 			showMessage = true;
 			comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 			cancel = true;
