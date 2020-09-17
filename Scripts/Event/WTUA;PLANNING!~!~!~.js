a59088e23b84b6adfec76e7626dd6d9f45556ad0
bushatos <66446858+bushatos@users.noreply.github.com>
2020-09-17 09:27:26 -0400

    per testing updates

--- Scripts/Event/WTUA;PLANNING!~!~!~.js
+++ Scripts/Event/WTUA;PLANNING!~!~!~.js
@@ -2,22 +2,24 @@ try {
 //07-2020 Boucher 11p  and 82p - updated per Word Doc on 9-2020
 // for all Planning records - updating the Reviewers Due date based on the Special Consideration field, only those active Reviews (can get overwritten by TRC Set Hearing Date)
 	if (matches(wfTask,'Review Distribution') && matches(wfStatus,'Routed for Review','Routed for Commercial Review','Routed for Residential Review','Routed for Residential and Commercial','Routed for Towers Review','Manual Routing')) {
-		var workflowTasks = aa.workflow.getTasks(capId).getOutput();
-		var taskAuditArray = ['Airport Review','Assessor Review','Building Inspection Review','Budget Review','Community Enhancement Review','County Library Review','Chesterfield Historical Society Review','Department of Health Review','CDOT Review','Economic Development Review','Environmental Engineering Review','Fire and Life Safety Review','GIS-EDM Utilities Review','GIS-IST Review','Parks and Recreation Review','Planning Review','Police Review','Real Property Review','School Research and Planning Review','County Attorney Review','Utilities Review','VDOT Review','Water Quality Review','Technical Review Committee','Staff and Developer Meeting'];
-		for (var ind in taskAuditArray) {
-			var wfaTask = taskAuditArray[ind];
-			for (var i in workflowTasks) {
-				var wfbTask = workflowTasks[i];
-				if (wfbTask.getActiveFlag() == 'Y') {
-					if (wfaTask == wfbTask.getTaskDescription()) {
-						if (AInfo['Special Consideration'] == 'Expedited') {
-						editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,10,true));
-						} else if (AInfo['Special Consideration'] == 'Fast Track') {
-						editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,5,true));
-						} else if (AInfo['Special Consideration'] == 'Regular') {
-						editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,15,true));
+		if (appMatch('*/Subdivision/*/*') || appMatch('*/SitePlan/*/*') {
+			var workflowTasks = aa.workflow.getTasks(capId).getOutput();
+			var taskAuditArray = ['Airport Review','Assessor Review','Building Inspection Review','Budget Review','Community Enhancement Review','County Library Review','Chesterfield Historical Society Review','Department of Health Review','CDOT Review','Economic Development Review','Environmental Engineering Review','Fire and Life Safety Review','GIS-EDM Utilities Review','GIS-IST Review','Parks and Recreation Review','Planning Review','Police Review','Real Property Review','School Research and Planning Review','County Attorney Review','Utilities Review','VDOT Review','Water Quality Review','Technical Review Committee','Staff and Developer Meeting'];
+			for (var ind in taskAuditArray) {
+				var wfaTask = taskAuditArray[ind];
+				for (var i in workflowTasks) {
+					var wfbTask = workflowTasks[i];
+					if (wfbTask.getActiveFlag() == 'Y') {
+						if (wfaTask == wfbTask.getTaskDescription()) {
+							if (AInfo['Special Consideration'] == 'Expedited') {
+							editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,10,true));
+							} else if (AInfo['Special Consideration'] == 'Fast Track') {
+							editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,5,true));
+							} else if (AInfo['Special Consideration'] == 'Regular') {
+							editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,15,true));
+							}
+						else { editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,15,true)); }
 						}
-					else { editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,15,true)); }
 					}
 				}
 			}
