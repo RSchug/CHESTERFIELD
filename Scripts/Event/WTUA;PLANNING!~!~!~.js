78b2eea52c28deaa5397b9f1e7395c01223dc0c5
bushatos <66446858+bushatos@users.noreply.github.com>
2020-09-17 09:35:27 -0400

    clean up

--- Scripts/Event/WTUA;PLANNING!~!~!~.js
+++ Scripts/Event/WTUA;PLANNING!~!~!~.js
@@ -2,7 +2,7 @@ try {
 //07-2020 Boucher 11p  and 82p - updated per Word Doc on 9-2020
 // for all Planning records - updating the Reviewers Due date based on the Special Consideration field, only those active Reviews (can get overwritten by TRC Set Hearing Date)
 	if (matches(wfTask,'Review Distribution') && matches(wfStatus,'Routed for Review','Routed for Commercial Review','Routed for Residential Review','Routed for Residential and Commercial','Routed for Towers Review','Manual Routing')) {
-		if (appMatch('*/Subdivision/*/*') || appMatch('*/SitePlan/*/*') {
+		if (appMatch('*/Subdivision/*/*') || appMatch('*/SitePlan/*/*')) {
 			var workflowTasks = aa.workflow.getTasks(capId).getOutput();
 			var taskAuditArray = ['Airport Review','Assessor Review','Building Inspection Review','Budget Review','Community Enhancement Review','County Library Review','Chesterfield Historical Society Review','Department of Health Review','CDOT Review','Economic Development Review','Environmental Engineering Review','Fire and Life Safety Review','GIS-EDM Utilities Review','GIS-IST Review','Parks and Recreation Review','Planning Review','Police Review','Real Property Review','School Research and Planning Review','County Attorney Review','Utilities Review','VDOT Review','Water Quality Review','Technical Review Committee','Staff and Developer Meeting'];
 			for (var ind in taskAuditArray) {
@@ -161,20 +161,21 @@ try {
 			}
 		}	  
 	}
+//per the ELM Planning Due Dates
 	if (matches(wfTask,'BOS Hearing') && matches(wfStatus,'Set Hearing Date')) {
 		if (appMatch('*/LandUse/ZoningCase/*') || appMatch('*/LandUse/HistoricPreservation/*') || appMatch('*/LandUse/SubstantialAccord/*')) {
 			
 			if (isTaskActive('Public Notices')) {
-				editTaskDueDate('Public Notices', dateAdd(getTaskDueDate('BOS Hearing'),1));
+				editTaskDueDate('Public Notices', dateAdd(getTaskDueDate('CPC Hearing'),1));
 			}
 			if (isTaskActive('Adjacents')) {
-				editTaskDueDate('Adjacents', dateAdd(getTaskDueDate('BOS Hearing'),2));
+				editTaskDueDate('Adjacents', dateAdd(getTaskDueDate('CPC Hearing'),2));
 			}
 			if (isTaskActive('IVR Message')) {
-				editTaskDueDate('IVR Message', dateAdd(getTaskDueDate('BOS Hearing'),5));
+				editTaskDueDate('IVR Message', dateAdd(getTaskDueDate('CPC Hearing'),5));
 			}
 			if (isTaskActive('BOS Staff Report')) {
-				editTaskDueDate('BOS Staff Report', dateAdd(getTaskDueDate('BOS Hearing'),-2));
+				editTaskDueDate('BOS Staff Report', dateAdd(getTaskDueDate('CPC Hearing'),-2));
 			}
 		}
 		else if (appMatch('*/LandUse/ManufacturedHomes/*') || appMatch('*/LandUse/RPAException/*')) {
