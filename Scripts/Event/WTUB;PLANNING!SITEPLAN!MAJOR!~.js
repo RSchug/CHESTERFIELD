0043682b72ad2bf406d3728c3e83fa4d5d58502c
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-19 17:26:54 -0400

    add No Time Limit field

--- Scripts/Event/WTUB;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/WTUB;PLANNING!SITEPLAN!MAJOR!~.js
@@ -1,5 +1,6 @@
 // Results and Ped Info required 
 if (matches(wfTask, 'CPC Hearing') && matches(wfStatus, 'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
+    if (AInfo['No Time Limit'] != 'CHECKED'){
         if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Non-Residential Gross Building Square Feet'] == null
         || AInfo['Number of Single Family Units Approved'] == null || AInfo['Number of Multi Family Units Approved'] == null || AInfo['Total Pedestrian Paths'] == null || AInfo['Shared Use Path Width'] == null
 		|| AInfo['Shared Use Path'] == null || AInfo['Pedestrian Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Sidewalk Width'] == null || AInfo['Sidewalks'] == null) {
@@ -8,9 +9,11 @@ if (matches(wfTask, 'CPC Hearing') && matches(wfStatus, 'CPC Approved','CPC Appr
             cancel = true;
         }
     }
+}
 
 // Results and Ped Info required 93P
 if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Approval')) {
+    if (AInfo['No Time Limit'] != 'CHECKED'){
     if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Non-Residential Gross Building Square Feet'] == null
     || AInfo['Number of Single Family Units Approved'] == null || AInfo['Number of Multi Family Units Approved'] == null || AInfo['Total Pedestrian Paths'] == null || AInfo['Shared Use Path Width'] == null
 	|| AInfo['Shared Use Path'] == null || AInfo['Pedestrian Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Sidewalk Width'] == null || AInfo['Sidewalks'] == null) {
@@ -18,4 +21,5 @@ if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Appro
         comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
         cancel = true;
     }
+}
 }
