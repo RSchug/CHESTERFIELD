4c42a870c5e1be51dfc8fded3202f80febae345f
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-19 18:07:00 -0400

    no time limit

--- Scripts/Event/WTUB;PLANNING!SUBDIVISION!PRELIMINARY!~.js
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!PRELIMINARY!~.js
@@ -1,5 +1,6 @@
 // Results and Ped Info required 93P
 if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Approval')) {
+    if (AInfo['No Time Limit'] != 'CHECKED'){
     if (AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Number of Single Family Units Approved'] == null
 	|| AInfo['Total Pedestrian Paths'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Pedestrian Width'] == null || AInfo['Pedestrian Trails'] == null
 	|| AInfo['Sidewalk Width'] == null || AInfo['Sidewalks'] == null) {
@@ -7,4 +8,5 @@ if (matches(wfTask, 'Administrative Approval') && matches(wfStatus, 'Final Appro
         comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
         cancel = true;
     }
+}
 }
