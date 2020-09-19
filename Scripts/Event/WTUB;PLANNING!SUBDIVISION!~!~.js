3e76896274f11a6ab49fbea94d1af699eddc9694
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-19 18:05:21 -0400

    no time limit

--- Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
@@ -1,6 +1,7 @@
 try {
 	// CPC Hearing Results required for Subdivisions
 	if (appMatch('*/*/ConstructionPlan/NA')|| appMatch('*/*/OverallConceptualPlan/NA')|| appMatch('*/*/ExceptiontoPreliminary/NA')){
+	    if (AInfo['No Time Limit'] != 'CHECKED'){	
 		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
 				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null) {
 					showMessage = true;
@@ -8,15 +9,18 @@ try {
 					cancel = true;
 				}
 			}
+		}
 	}
 	if (appMatch('*/*/Preliminary/NA')){
 		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
-				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null){
+			if (AInfo['No Time Limit'] != 'CHECKED'){
+			if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null){
 					showMessage = true;
 					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
 					cancel = true;
 				}
 			}
+		}
 	}
 	} catch (err) {
 		logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
