903ab73483b039b60ef560bd4f236de1356114e5
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-22 19:55:32 -0400

    case complete

--- Scripts/Event/WTUB;PLANNING!SITEPLAN!MINOR!~.js
+++ Scripts/Event/WTUB;PLANNING!SITEPLAN!MINOR!~.js
@@ -13,6 +13,14 @@ try {
             }
         }
     }
+		if (matches(wfTask,'Case Complete') && matches(wfStatus,'Closed')) {
+            if (AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null 
+            || AInfo['Approved Time Limit'] == null || AInfo['Conditions'] == null || AInfo['Number of Single Family Units Approved'] == null || AInfo['Number of Town House Units Approved'] == null || AInfo['Number of Multi Family Units Approved'] == null || AInfo['Non-Residential Gross Building Square Feet'] == null){
+					showMessage = true;
+					comment('You cannot advance this workflow until the Results and Pedestrian Paths Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
+					cancel = true;
+				}
+			}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
