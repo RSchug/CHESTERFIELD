ad67dd24afd9b407261fed3bd88f38c5dce63162
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-23 14:02:00 -0400

    remove denied

--- Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
+++ Scripts/Event/WTUB;PLANNING!SUBDIVISION!~!~.js
@@ -2,7 +2,7 @@ try {
 	// CPC Hearing Results required for Subdivisions
 	if (appMatch('*/*/ConstructionPlan/NA')|| appMatch('*/*/OverallConceptualPlan/NA')|| appMatch('*/*/ExceptiontoPreliminary/NA')){
 	    if (AInfo['No Time Limit'] != 'CHECKED'){	
-		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
+		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review')) { //Denied removed on 9/23/2020 per request
 				if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null) {
 					showMessage = true;
 					comment('You cannot advance this workflow until ALL fields in the <b>Results</b> area of the Data Fields are completely filled in.  Put in zeroes (0) for those fields that do not apply.');
@@ -12,7 +12,7 @@ try {
 		}
 	}
 	if (appMatch('*/*/Preliminary/NA')){
-		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review','CPC Denied')) {
+		if (matches(wfTask,'CPC Hearing') && matches(wfStatus,'CPC Approved','CPC Approved with Admin Review')) { //Denied removed on 9/23/2020 per request
 			if (AInfo['No Time Limit'] != 'CHECKED'){
 			if (AInfo['Conditions'] == null || AInfo['Approved Time Limit'] == null || AInfo['Sidewalks'] == null || AInfo['Sidewalk Width'] == null || AInfo['Pedestrian Trails'] == null || AInfo['Pedestrian Width'] == null || AInfo['Shared Use Path'] == null || AInfo['Shared Use Path Width'] == null || AInfo['Total Pedestrian Paths'] == null){
 					showMessage = true;
