57b6888bb92ec00974cc516765249a77f8ceb470
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-23 15:00:18 -0400

    comment out

--- Scripts/Event/ISA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ISA;BUILDING!PERMIT!~!~.js
@@ -1,20 +1,20 @@
-comment("Test form ISA");
-comment("Get the Inspection Count for this type");
-var inspResult = aa.inspection.getInspection(capId, inspId);
-inspObj = inspResult.getOutput();
-inspObj.setTimeTotal(Number(getinsptypecount(capId, inspType)));
-var result = aa.inspection.editInspection(inspObj);
-
-if (getLastInspectioncomment(inspType) != "No Comments") {
-	var reqcomment = getInspectionComment(capId, inspId);
-	if (reqcomment != "No Comment" && reqcomment != null) {
-		inspcomment = reqcomment + " Last Result: " + getLastInspectioncomment(inspType);
-		editInspectionComment(capId, inspId, inspcomment);
-	}
-	else {
-		editInspectionComment(capId, inspId, getLastInspectioncomment(inspType));
-	}
-}
+//comment("Test form ISA"); moved to ISA Building
+//comment("Get the Inspection Count for this type");
+//var inspResult = aa.inspection.getInspection(capId, inspId);
+//inspObj = inspResult.getOutput();
+//inspObj.setTimeTotal(Number(getinsptypecount(capId, inspType)));
+//var result = aa.inspection.editInspection(inspObj);
+//
+//if (getLastInspectioncomment(inspType) != "No Comments") {
+//	var reqcomment = getInspectionComment(capId, inspId);
+//	if (reqcomment != "No Comment" && reqcomment != null) {
+//		inspcomment = reqcomment + " Last Result: " + getLastInspectioncomment(inspType);
+//		editInspectionComment(capId, inspId, inspcomment);
+//	}
+//	else {
+//		editInspectionComment(capId, inspId, getLastInspectioncomment(inspType));
+//	}
+//}
 //When Framing Inspection Type is scheduled, schedule a VSMP Inspection Type for the following day and assign to EE Inspector
 //if (matches(inspType, "Framing")) {
 //	scheduleInspectDate("VSMP", dateAdd(inspSchedDate, 1), currentUserID, null, "Auto Scheduled from Scheduled Framing Inspection");
