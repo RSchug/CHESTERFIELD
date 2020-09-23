b0530fd3e6f6ef45dbd3141317faf5c218e52503
bushatos <66446858+bushatos@users.noreply.github.com>
2020-09-23 05:43:31 -0400

    Update per testing on Due Dates

--- Scripts/Event/WTUA;PLANNING!LANDUSE!APPEAL!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!APPEAL!~.js
@@ -1,15 +1,24 @@
 try {
-    // Set the Reviewers Tasks per the wfStatus choosen per REVIEW DEPTS FOR ELM Spreadsheet scritp# 60p
+    // Set the Reviewers Tasks per the wfStatus choosen per REVIEW DEPTS FOR ELM Spreadsheet scritp# 60p - 09-2020 
 	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Review') {
 		activateTask("CDOT Review");
-		activateTask("County Attorney Review");		
+		editTaskDueDate('CDOT Review', dateAdd(getTaskDueDate('Review Distribution'),14));
+		activateTask("County Attorney Review");
+		editTaskDueDate('County Attorney Review', dateAdd(getTaskDueDate('Review Distribution'),14));		
 		activateTask("Environmental Engineering Review");
+		editTaskDueDate('Environmental Engineering Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("Fire and Life Safety Review");
+		editTaskDueDate('Fire and Life Safety Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("Department of Health Review");
+		editTaskDueDate('Department of Health Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("Planning Review");
+		editTaskDueDate('Planning Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("Utilities Review");
+		editTaskDueDate('Utilities Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("VDOT Review");
+		editTaskDueDate('VDOT Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		activateTask("Real Property Review");
+		editTaskDueDate('Real Property Review', dateAdd(getTaskDueDate('Review Distribution'),14));
 		deactivateTask("Default");
 	}
 } catch (err) {
