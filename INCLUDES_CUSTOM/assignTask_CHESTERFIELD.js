db6c3850790ca047b76c31f22f961c7f1288246c
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-25 14:28:05 -0400

    CE Council Dist

--- INCLUDES_CUSTOM/assignTask_CHESTERFIELD.js
+++ INCLUDES_CUSTOM/assignTask_CHESTERFIELD.js
@@ -18,6 +18,7 @@ function assignTask_CHESTERFIELD(wfstr, username) {
     var gisLayerName = null, gisLayerAbbr = null, gisLayerField = null;
     if (typeof (gisMapService) == "undefined") gisMapService = null; // Check for global.
     if (userDiscipline == "Enforcement") {
+        userDistrict = AInfo["ParcelAttribute.CouncilDist"];
         gisLayerName = "Enforcement Boundaries";
         gisLayerField = "InspectorID";
         logDebug("Using userDiscipline: " + userDiscipline + ", param");
@@ -31,6 +32,7 @@ function assignTask_CHESTERFIELD(wfstr, username) {
         if (appMatch("Enforcement/*/*/*")) {
             logDebug("Using userDiscipline: " + userDiscipline + " appTypeArray[0]");
             userDiscipline = "Enforcement";
+            userDistrict = AInfo["ParcelAttribute.CouncilDist"];
             gisLayerName = "Enforcement Boundaries";
             gisLayerField = "InspectorID";
             logDebug("Using userDiscipline: " + userDiscipline + ", appType: Enforcement");
