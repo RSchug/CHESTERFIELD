054f4ffc6cba053b84b9f3f1e041369d31feb677
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-09-29 16:01:43 -0400

    added publicuser

--- Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ISB;BUILDING!PERMIT!~!~.js
@@ -1,13 +1,16 @@
-// Permit must be Issued 
+// Permit must be Issued for ACA and Selectron
+if (publicUser) {
 if (!wasCapStatus(["Issued"])) { //Remove != "Site Visit" as not available in IVR, "Temporary CO Issued" removed as always Issued first
         showMessage = true;
         comment('<font size=small><b>Record must be Issued to Schedule Inspection</b></font>');
         // if (!exists(vEventName, ["InspectionMultipleScheduleAfter", "InspectionMultipleScheduleBefore"])) 
         cancel = true;
 }
-// Parent Permit must be Issued
+}
+// Parent Permit must be Issued for ACA and Selectron
 //if (inspType != "Site Visit") { //removed as not needed for IVR
         var parentAppTypes = null;
+        if (publicUser) {
         if (exists(appTypeArray[3], ["Boiler", "Fire", "Gas", "Mechanical", "Plumbing"])) {
                 var parentAppTypes = ["Building/Permit/Commercial/NA", "Building/Permit/Residential/NA", "Building/Permit/Residential/Multi-Family"];
         } else if (exists(appTypeArray[3], ["Electrical"])) { // Includes Elevator & Sign 
@@ -24,3 +27,4 @@ if (!wasCapStatus(["Issued"])) { //Remove != "Site Visit" as not available in IV
                 // if (!exists(vEventName, ["InspectionMultipleScheduleAfter", "InspectionMultipleScheduleBefore"])) 
                 cancel = true;
         }
+}
