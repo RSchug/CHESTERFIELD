3397837ddfc8f41c37c2c51786abcd2e68394bd8
bushatos <66446858+bushatos@users.noreply.github.com>
2020-10-08 22:47:56 -0400

    script # 105aca

--- Scripts/Event/ASA;PLANNING!~!~!~.js
+++ Scripts/Event/ASA;PLANNING!~!~!~.js
@@ -49,6 +49,50 @@ try {
 	if (AInfo['Submittal Count'] == null) {
 		editAppSpecific('Submittal Count',1);
 	}
+	//10-2020 Boucher 105aca
+		var addrArray = [];
+		loadAddressAttributesInternal(paArray);
+		var TechRev = addrArray["AddressAttribute.County"];
+	
+	if (TechRev != null) {
+		addStdCondition('Economic Development','Eligible for Technology Zone Incentive Program');
+		email('dboucher@truepointsolutions.com','noreply@chesterfield.gov','Record: ' + capId + ' submitted in the Tech Zone','Date: ' + fileDate + ' For Record Type: ' + appTypeAlias);
+	}
 } catch (err) {
 		logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
+function loadAddressAttributesInternal(thisArr)
+{
+	var itemCap = capId;
+	if (arguments.length == 2) itemCap = arguments[1]; // use cap ID specified in args
+
+	var fcapAddressObj = null;
+   	var capAddressResult = aa.address.getAddressWithAttributeByCapId(itemCap);
+   	if (capAddressResult.getSuccess())
+   		var fcapAddressObj = capAddressResult.getOutput();
+   	else
+     		logDebug("**ERROR: Failed to get Address object: " + capAddressResult.getErrorType() + ":" + capAddressResult.getErrorMessage())
+
+  	for (i in fcapAddressObj)
+  	{
+  		addressAttrObj = fcapAddressObj[i].getAttributes().toArray();
+  		for (z in addressAttrObj)
+			thisArr["AddressAttribute." + addressAttrObj[z].getB1AttributeName()]=addressAttrObj[z].getB1AttributeValue();
+
+		// Explicitly load some standard values
+		thisArr["AddressAttribute.PrimaryFlag"] = fcapAddressObj[i].getPrimaryFlag();
+		thisArr["AddressAttribute.HouseNumberStart"] = fcapAddressObj[i].getHouseNumberStart();
+		thisArr["AddressAttribute.StreetDirection"] = fcapAddressObj[i].getStreetDirection();
+		thisArr["AddressAttribute.StreetName"] = fcapAddressObj[i].getStreetName();
+		thisArr["AddressAttribute.StreetSuffix"] = fcapAddressObj[i].getStreetSuffix();
+		thisArr["AddressAttribute.City"] = fcapAddressObj[i].getCity();
+		thisArr["AddressAttribute.State"] = fcapAddressObj[i].getState();
+		thisArr["AddressAttribute.Zip"] = fcapAddressObj[i].getZip();
+		thisArr["AddressAttribute.AddressStatus"] = fcapAddressObj[i].getAddressStatus();
+		thisArr["AddressAttribute.County"] = fcapAddressObj[i].getCounty();
+		thisArr["AddressAttribute.Country"] = fcapAddressObj[i].getCountry();
+		thisArr["AddressAttribute.AddressDescription"] = fcapAddressObj[i].getAddressDescription();
+		thisArr["AddressAttribute.XCoordinate"] = fcapAddressObj[i].getXCoordinator();
+		thisArr["AddressAttribute.YCoordinate"] = fcapAddressObj[i].getYCoordinator();
+  	}
 }
