379e352ab347d48f4bb4cf1b5d59b4f32be30a46
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-10-11 18:30:53 -0400

    !=null

--- Scripts/Event/CTRCA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/CTRCA;BUILDING!PERMIT!~!~.js
@@ -1,2 +1,4 @@
-addParent(AInfo["Related Record ID"]);
-pLicCap  = aa.cap.getCapID(["Related Record ID"]).getOutput();
+if (AInfo["Related Record ID"] != null) {
+    addParent(AInfo["Related Record ID"]);
+    pLicCap  = aa.cap.getCapID(["Related Record ID"]).getOutput();
+}
