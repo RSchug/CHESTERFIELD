7dc197d5d1c95379f0cff1ff4eda5bcb2fd32065
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-10-14 20:51:43 -0400

    publicuser

--- Scripts/Event/ASA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
+++ Scripts/Event/ASA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
@@ -1,3 +1,5 @@
+if (!publicUser) {
 addFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",houseCount,"Y");
 addFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
 addFee("EESITE","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
+}
