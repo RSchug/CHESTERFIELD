82b16c34fb3322266fd4c001d7e86aff4ff4dc2a
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-10-27 10:33:07 -0400

    ADD FEES

--- Scripts/Event/ASA;UTILITIES!RESIDENTIALCOMPANIONMETER!NA!NA.js
+++ Scripts/Event/ASA;UTILITIES!RESIDENTIALCOMPANIONMETER!NA!NA.js
@@ -4,3 +4,10 @@ editAppSpecific("Service Number",lookup("NEW_TAP_NUMBER","TAP_NUMBER"));
 editLookup("NEW_TAP_NUMBER","TAP_NUMBER",zeroPad(newTapNumber+count,8));
 
 if (!publicUser) performCISLookup()
+//Add Fees//
+if (!publicUser) {
+    addFee("CC-UTL-RM01","CC-UTL-RM","FINAL",1,"Y");
+    addFee("CC-UTL-RM02","CC-UTL-RM","FINAL",1,"Y");
+    addFee("BACKFLOW","CC-UTL-RM","FINAL",1,"Y");
+    addFee("STATELEVY","CC-UTL-RM","FINAL",1,"Y")
+}
