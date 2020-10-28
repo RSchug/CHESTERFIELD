3750bd762ae2a222da6ec9a144527828620896aa
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-10-27 20:34:35 -0400

    Documents Submitted online

--- Scripts/Event/WTUA;BUILDING!~!~!~.js
+++ Scripts/Event/WTUA;BUILDING!~!~!~.js
@@ -6,3 +6,23 @@
 	}
 //For DigEplan
 	loadCustomScript("WTUA_EXECUTE_DIGEPLAN_SCRIPTS_BUILD");
+//Adhoc task updated to Amendment then update Status 'Amendment Submitted' on Inspections, Certificate Issuance or Certificate of Occupancy
+if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
+	if (isTaskActive('Inspections'){
+		updateTask("Inspections", "Amendment Submitted", "Updated based on Docuent Submitted Online 'Amendment' Status", "");
+	}
+}
+if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
+	if (isTaskActive('Certificate Issuance'){
+		updateTask("Certificate Issuance", "Amendment Submitted", "Updated based on Docuent Submitted Online 'Amendment' Status", "");
+	}
+}
+if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
+	if (isTaskActive('Certificate of Occupancy'){
+		updateTask("Certificate of Occupancy", "Amendment Submitted", "Updated based on Docuent Submitted Online 'Amendment' Status", "");
+	}
+}
+//Adhoc task updated to Revision then activate 'Review Distribution' and status of 'Corrections Received'
+if (wfTask =='Document Submitted Online' && wfStatus == 'Revision'){
+		updateTask("Review Distribution", "Corrections Received", "Updated based on Docuent Submitted Online 'Revision' Status", "");
+	}
