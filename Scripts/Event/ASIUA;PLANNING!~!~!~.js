3428068d171389839b2feddbbb51d410f1a0fe58
bushatos <66446858+bushatos@users.noreply.github.com>
2020-11-04 23:01:04 -0500

    still testing..

--- Scripts/Event/ASIUA;PLANNING!~!~!~.js
+++ Scripts/Event/ASIUA;PLANNING!~!~!~.js
@@ -24,73 +24,15 @@ try {
 		editAppSpecific("Total Parcel Acreage", parcelAcreage);
 		editAppSpecific("Total application acreage", revisedAcreage);
 		editAppSpecific("Total number of parcels", countParcels);
-	}
+
 //11-2020 added code for copying Address Parcel Owner information from a Table to a Record, because we cannont have multiple parcel submission at ACA intake
-	if (typeof (ParcelValidatedNumber) == "undefined") {
-		ParcelValidatedNumber = null;
-		loadASITables4ACA_TPS();// Load ASITables into Arrays
-	}
-	if (typeof (CCPLNSTMN) != "undefined") { // Check if ASITable array exists - this is site plan Major and Minor
-		logDebug("ASITable: CCPLNSTMN");
-		var tablename = CCPLNSTMN;
-	}
-	else if (typeof (CCGIS) != "undefined") { // Check if ASITable array exists - this is most others
-		logDebug("ASITable: CCGIS");
-		var tablename = CCGIS;
-	}
-	else { cancel = true; }
-	for (var rr in tablename) {
-		var eachRow = tablename[rr];
-		var chkcreate = (eachRow["Create Address-Parcel-Owner"] ? eachRow["Create Address-Parcel-Owner"] :"");
-		var BaseAddress = eachRow["Base Address"];
-		logDebug("tablename["+rr+"][Create Address-Parcel-Owner]:"+chkcreate);
-		if (chkcreate == 'CHECKED') {
-			addParcelAndOwnerFromRefAddress(BaseAddress);
+		for (b in tempAsit) {
+			if (tempAsit[b]["Create Address-Parcel-Owner"]) == 'CHECKED') {
+				var BaseAddress = tempAsit[b]["Base Address"];
+				addParcelAndOwnerFromRefAddress(BaseAddress);
+			}
 		}
 	}
-
 } catch (err) {
 		logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
-}
-
-function loadASITables4ACA_TPS() {
- 	// Loads App Specific tables into their own array of arrays.  Creates global array objects
-	// Optional parameter, cap ID to load from.  If no CAP Id specified, use the capModel
-	var itemCap = capId;
-	if (arguments.length == 1) {
-		itemCap = arguments[0]; // use cap ID specified in args
-		var gm = aa.appSpecificTableScript.getAppSpecificTableGroupModel(itemCap).getOutput();
-	}
-	else {
-		var gm = cap.getAppSpecificTableGroupModel()
-	}
-	var ta = gm.getTablesMap();
-	var tai = ta.values().iterator();
-	while (tai.hasNext()) {
-		var tsm = tai.next();
-		if (tsm.rowIndex.isEmpty()) continue;  // empty table
-			var tempObject = new Array();
-			var tempArray = new Array();
-			var tn = tsm.getTableName();
-			tn = String(tn).replace(/[^a-zA-Z0-9]+/g,'');
-		if (!isNaN(tn.substring(0,1))) tn = "TBL" + tn  // prepend with TBL if it starts with a number
-			var tsmfldi = tsm.getTableField().iterator();
-			var tsmcoli = tsm.getColumns().iterator();
-			var numrows = 1;
-		while (tsmfldi.hasNext()) { // cycle through fields
-			if (!tsmcoli.hasNext()) { // cycle through columns
-				var tsmcoli = tsm.getColumns().iterator();
-				tempArray.push(tempObject);  // end of record
-				var tempObject = new Array();  // clear the temp obj
-				numrows++;
-			}
-			var tcol = tsmcoli.next();
-			var tval = tsmfldi.next();  //.getInputValue();
-			tempObject[tcol.getColumnName()] = tval;
-		}
-	tempArray.push(tempObject);  // end of record
-	var copyStr = "" + tn + " = tempArray";
-	logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)");
-	eval(copyStr);  // move to table name
-	}
 }
