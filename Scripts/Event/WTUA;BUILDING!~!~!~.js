40e5cb9dc1953657e8454aebbdd40c1426adf49b
mbouquin <66886147+mbouquin@users.noreply.github.com>
2020-11-05 18:40:04 -0500

    DSO Inspections for Residential

--- Scripts/Event/WTUA;BUILDING!~!~!~.js
+++ Scripts/Event/WTUA;BUILDING!~!~!~.js
@@ -10,6 +10,29 @@
 if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
 	if (isTaskActive('Inspections')){
 		updateTask("Inspections", "Amendment Submitted", "Updated based on Docuent Submitted Online 'Amendment' Status", "");
+			var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
+			if (appMatch("Building/Permit/Residential/NA"))
+				var newAppTypeString = "Building/Permit/Residential/Amendment";
+			var newCapName = capName;
+			var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
+			var newCapRelation = "Child";
+			var srcCapId = capId;
+			var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
+			if (newCapId) {
+				showMessage = true;
+				comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
+					+ "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
+				if (wfComment && wfComment != "") {
+					cWorkDesc = workDescGet(capId);
+					nWorkDesc = cWorkDesc + ", " + wfComment;
+					updateWorkDesc(nWorkDesc, newCapId);
+				}
+				// set the Permit Expiration Date to 180 days from Application Date.
+				var expDateField = "Permit Expiration Date";
+				var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
+				editAppSpecific(expDateField, expDate, newCapId);
+			}
+			addFee("ADMIN", "CC-BLD-ADMIN", "FINAL", 1, "Y");
 	}
 }
 if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
