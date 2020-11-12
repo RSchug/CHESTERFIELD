da51edf6831b1c92795f44a467819b35cfb86ce6
bushatos <66446858+bushatos@users.noreply.github.com>
2020-11-12 09:54:44 -0500

    fix..

--- Scripts/Event/WTUA;PLANNING!LANDUSE!SPECIALEXCEPTION!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!SPECIALEXCEPTION!~.js
@@ -7,7 +7,7 @@ try {
 		addFee("SEOTHER","CC-PLANNING","FINAL",1,"N");
 	}
 	else if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Application Type"] == "Amendment")){
-		addFee("AMEND5","CC-PLANNING","FINAL",1,"N");
+		addFee("AMEND5SE","CC-PLANNING","FINAL",1,"N");
 	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
