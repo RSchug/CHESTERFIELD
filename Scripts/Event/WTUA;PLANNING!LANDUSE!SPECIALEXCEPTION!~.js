0dd7db719129458d9e8496da72c4b0e94bcf2ca0
bushatos <66446858+bushatos@users.noreply.github.com>
2020-11-12 09:27:29 -0500

    clean up...

--- Scripts/Event/WTUA;PLANNING!LANDUSE!SPECIALEXCEPTION!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!SPECIALEXCEPTION!~.js
@@ -1,7 +1,14 @@
+try {
 //Fees for Special Exception
-if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Special Exception Request Type"] == "Temporary Manufactured Home")) {
-	addFee("SEMANUFACTUR","CC-PLANNING","FINAL",1,"Y");
-}
-if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Special Exception Request Type"] != "Temporary Manufactured Home")){
-	addFee("SEOTHER","CC-PLANNING","FINAL",1,"N");
+	if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Special Exception Request Type"] == "Temporary Manufactured Home") && (AInfo["Application Type"] != "Amendment")) {
+		addFee("SEMANUFACTUR","CC-PLANNING","FINAL",1,"Y");
+	}
+	else if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Special Exception Request Type"] != "Temporary Manufactured Home") && (AInfo["Application Type"] != "Amendment")){
+		addFee("SEOTHER","CC-PLANNING","FINAL",1,"N");
+	}
+	else if ((wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') && (AInfo["Application Type"] == "Amendment")){
+		addFee("AMEND5","CC-PLANNING","FINAL",1,"N");
+	}
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
