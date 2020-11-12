7e653c1a185fddc5ab0827b509ac88ce96327939
bushatos <66446858+bushatos@users.noreply.github.com>
2020-11-11 22:02:23 -0500

    clean up

--- Scripts/Event/CTRCA;PLANNING!SITEPLAN!MAJOR!~.js
+++ Scripts/Event/CTRCA;PLANNING!SITEPLAN!MAJOR!~.js
@@ -1,6 +1,8 @@
 try {
-	var pLicCap  = aa.cap.getCapID(["Case Number"]).getOutput();
-	addParent(AInfo["Case Number"]);
+	//var pLicCap  = aa.cap.getCapID(["Case Number"]).getOutput();
+	if (AInfo["Case Number"] != null) {
+		addParent(AInfo["Case Number"]);
+	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
