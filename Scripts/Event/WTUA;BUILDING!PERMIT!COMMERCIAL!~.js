f197cb11192f817d82766c55a2af4c8a3dace38b
bushatos <66446858+bushatos@users.noreply.github.com>
2020-11-19 08:35:22 -0500

    clean up for Building Com - per MB

--- Scripts/Event/WTUA;BUILDING!PERMIT!COMMERCIAL!~.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!COMMERCIAL!~.js
@@ -1,28 +1,38 @@
-if (!appMatch("Building/Permit/Commercial/Demolition")){
-if ((wfTask =='Inspections' || wfTask == 'Certificate Issuance') && wfStatus == 'Amendment Submitted') {
-    var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
-    if (appMatch("Building/Permit/Commercial/NA"))
-        var newAppTypeString = "Building/Permit/Commercial/Amendment";
-    else if (appMatch("Building/Permit/Commercial/*"))
-        var newAppTypeString = "Building/Permit/Commercial/AmendmentTrade";
-    var newCapName = capName;
-    var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
-    var newCapRelation = "Child";
-    var srcCapId = capId;
-    var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
-    if (newCapId) {
-        showMessage = true;
-        comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
-            + "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
-        if (wfComment && wfComment != "") {
-            cWorkDesc = workDescGet(capId);
-            nWorkDesc = cWorkDesc + ", " + wfComment;
-            updateWorkDesc(nWorkDesc, newCapId);
-        }
-        // set the Permit Expiration Date to 180 days from Application Date.
-        var expDateField = "Permit Expiration Date";
-        var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
-        editAppSpecific(expDateField, expDate, newCapId);
-    }
-}
+try {
+	if (!appMatch("Building/Permit/Commercial/Demolition")){
+		if ((wfTask =='Inspections' || wfTask == 'Certificate Issuance') && wfStatus == 'Amendment Submitted') {
+			var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
+			if (appMatch("Building/Permit/Commercial/NA"))
+				var newAppTypeString = "Building/Permit/Commercial/Amendment";
+			else if (appMatch("Building/Permit/Commercial/*"))
+				var newAppTypeString = "Building/Permit/Commercial/AmendmentTrade";
+			var newCapName = capName;
+			var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
+			var newCapRelation = "Child";
+			var srcCapId = capId;
+			var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
+			if (newCapId) {
+				showMessage = true;
+				comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
+					+ "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
+				if (wfComment && wfComment != "") {
+					cWorkDesc = workDescGet(capId);
+					nWorkDesc = cWorkDesc + ", " + wfComment;
+					updateWorkDesc(nWorkDesc, newCapId);
+				}
+				// set the Permit Expiration Date to 180 days from Application Date.
+				var expDateField = "Permit Expiration Date";
+				var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
+				editAppSpecific(expDateField, expDate, newCapId);
+			}
+		}
+	}
+	//11-2020 db Moved from WTUA:Building because should just be for Building Com record types
+	if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
+		if (isTaskActive('Certificate Issuance')){
+			updateTask("Certificate Issuance", "Amendment Submitted", "Updated based on Document Submitted Online 'Amendment' Status", "");
+		}
+	}
+} catch (err) {
+	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
--- Scripts/Event/WTUA;BUILDING!~!~!~.js
+++ Scripts/Event/WTUA;BUILDING!~!~!~.js
@@ -1,5 +1,5 @@
+try {
 // If setting the License status manually from the workflow
-
 	if (wfTask == 'Annual Status' && wfStatus == 'About to Expire') {
 		lic = new licenseObject(capIDString);
 		lic.setStatus('About to Expire');
@@ -7,42 +7,41 @@
 //For DigEplan
 	loadCustomScript("WTUA_EXECUTE_DIGEPLAN_SCRIPTS_BUILD");
 //Adhoc task updated to Amendment then update Status 'Amendment Submitted' on Inspections, Certificate Issuance or Certificate of Occupancy
-if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
-	if (isTaskActive('Inspections')){
-		updateTask("Inspections", "Amendment Submitted", "Updated based on Document Submitted Online 'Amendment' Status", "");
-			var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
-			if (appMatch("Building/Permit/Residential/NA"))
-				var newAppTypeString = "Building/Permit/Residential/Amendment";
-			var newCapName = capName;
-			var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
-			var newCapRelation = "Child";
-			var srcCapId = capId;
-			var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
-			if (newCapId) {
-				showMessage = true;
-				comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
-					+ "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
-				if (wfComment && wfComment != "") {
-					cWorkDesc = workDescGet(capId);
-					nWorkDesc = cWorkDesc + ", " + wfComment;
-					updateWorkDesc(nWorkDesc, newCapId);
+	if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
+		if (isTaskActive('Inspections')){
+			updateTask("Inspections", "Amendment Submitted", "Updated based on Document Submitted Online 'Amendment' Status", "");
+				var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
+				if (appMatch("Building/Permit/Residential/NA"))
+					var newAppTypeString = "Building/Permit/Residential/Amendment";
+				var newCapName = capName;
+				var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
+				var newCapRelation = "Child";
+				var srcCapId = capId;
+				var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
+				if (newCapId) {
+					showMessage = true;
+					comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
+						+ "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
+					if (wfComment && wfComment != "") {
+						cWorkDesc = workDescGet(capId);
+						nWorkDesc = cWorkDesc + ", " + wfComment;
+						updateWorkDesc(nWorkDesc, newCapId);
+					}
+					// set the Permit Expiration Date to 180 days from Application Date.
+					var expDateField = "Permit Expiration Date";
+					var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
+					editAppSpecific(expDateField, expDate, newCapId);
 				}
-				// set the Permit Expiration Date to 180 days from Application Date.
-				var expDateField = "Permit Expiration Date";
-				var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
-				editAppSpecific(expDateField, expDate, newCapId);
-			}
-			addFee("ADMIN", "CC-BLD-ADMIN", "FINAL", 1, "Y");
+				addFee("ADMIN", "CC-BLD-ADMIN", "FINAL", 1, "Y");
+		}
 	}
-}
-//if (wfTask =='Document Submitted Online' && wfStatus == 'Amendment'){
-//	if (isTaskActive('Certificate Issuance')){
-//		updateTask("Certificate Issuance", "Amendment Submitted", "Updated based on Document Submitted Online 'Amendment' Status", "");
-//	}
-//}
 //Adhoc task updated to Revision then activate 'Review Distribution' and status of 'Corrections Received'
-if (wfTask =='Document Submitted Online' && wfStatus == 'Revision'){
-	if (isTaskActive('Review Distribution')){
-		updateTask("Review Distribution", "Corrections Received", "Updated based on Document Submitted Online 'Revision' Status", "");
-		updateAppStatus("In Review","Updated based on Document Submitted Online 'Revision' Status.");
-	}}
+	if (wfTask =='Document Submitted Online' && wfStatus == 'Revision'){
+		if (isTaskActive('Review Distribution')){
+			updateTask("Review Distribution", "Corrections Received", "Updated based on Document Submitted Online 'Revision' Status", "");
+			updateAppStatus("In Review","Updated based on Document Submitted Online 'Revision' Status.");
+		}
+	}
+} catch (err) {
+	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
