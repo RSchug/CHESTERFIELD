553e0c26a05badd0539f70f59be083db9c11a75c
bushatos <66446858+bushatos@users.noreply.github.com>
2020-12-08 06:30:05 -0500

    clean up from Melissa direct updates

--- Scripts/Event/ASA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
+++ Scripts/Event/ASA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
@@ -1,5 +1,7 @@
-var NumberofUnits = AInfo["Number of Units"];
+if (!publicUser) {
+	var NumberofUnits = AInfo["Number of Units"];
 
-addFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",NumberofUnits,"Y");
-addFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
-addFee("EESITE","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
+	addFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",NumberofUnits,"Y");
+	addFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
+	addFee("EESITE","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
+}
--- Scripts/Event/ASA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/ASA;BUILDING!PERMIT!~!~.js
@@ -1,22 +1,23 @@
 try {
 // set the Permit Expiration Date to 180 days from Application Date.
-logDebug("fileDate: " + new Date(fileDate));
-var expField = "Permit Expiration Date";
-var expDateNew = jsDateToASIDate(new Date(dateAdd(fileDate,180)));
-logDebug("Updating " + expField + " to " + expDateNew);
-editAppSpecific(expField, expDateNew);
- 
+	logDebug("fileDate: " + new Date(fileDate));
+	var expField = "Permit Expiration Date";
+	var expDateNew = jsDateToASIDate(new Date(dateAdd(fileDate,180)));
+	logDebug("Updating " + expField + " to " + expDateNew);
+	editAppSpecific(expField, expDateNew);
+	 
 //update ASI Housing Units based on Additional Info Housing Units
-editAppSpecific("Number of Units", houseCount);
-
+	if (!publicUser) {
+		ditAppSpecific("Number of Units", houseCount);
+	}
 //If Is a contractor to be determined? is "Yes" then add Licensed Professional Type of "Contractor" and License # of "TBD"//
-if (AInfo["Is a contractor to be determined?"] == "Yes") {
-var lpType = "Contractor", lpId = "TBD", lpBoard = null, lpExpirDate = null, lpFirstName = "", lpLastName = "To Be Determined";
-createLP(lpId, lpType, capId, lpBoard, lpExpirDate, lpFirstName, null, lpLastName);
-}
-
-if (!publicUser) assignCap(currentUserID);
-
+	if (AInfo["Is a contractor to be determined?"] == "Yes") {
+		var lpType = "Contractor", lpId = "TBD", lpBoard = null, lpExpirDate = null, lpFirstName = "", lpLastName = "To Be Determined";
+		createLP(lpId, lpType, capId, lpBoard, lpExpirDate, lpFirstName, null, lpLastName);
+	}
+	if (!publicUser) {
+		assignCap(currentUserID);
+	}
 } catch (err) {
 		logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
