a459ef0b2dd7b177b86b774bb6f6a63a86b8a040
SYSTEM <SYSTEM@NONPROD-AA-0.us.accela.com>
2020-12-23 13:31:15 -0600

    commit by emse tools 2020-12-23 13:31:15

--- Scripts/_INCLUDES/INCLUDES_CUSTOM.js
+++ Scripts/_INCLUDES/INCLUDES_CUSTOM.js
@@ -6854,3 +6854,38 @@ function rowObj(rObj) {
     this.water =  rObj.a::Water.toString();
 }
 
+function schedulePendingInspection(iType,DateToSched)
+{
+	var itmResult = aa.inspection.getInspections(capId)
+	var itmArray = itmResult.getOutput();
+	
+	for (thisItm in itmArray)
+		{
+			var it = itmArray[thisItm];
+			if(it.getInspectionType() == iType && it.getInspectionStatus() == "Pending" && it.getScheduledDate() == null)
+			{
+			it.setScheduledDate(aa.date.parseDate(DateToSched))
+			aa.inspection.editInspection(it);
+			}
+		}
+}
+
+function checkinspectionstatus(iType,status)
+{
+	var check = "false";
+	
+	var itmResult = aa.inspection.getInspections(capId)
+	var itmArray = itmResult.getOutput();
+	
+	for (thisItm in itmArray)
+		{
+			var it = itmArray[thisItm];
+			if(it.getInspectionType() == iType && it.getInspectionStatus() == status)
+			{
+			check = "true";
+            return check
+			}
+		}
+	
+	return check
+}
