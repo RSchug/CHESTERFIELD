8f3647ee0e59a78753c5c3cd28783286eee7d9c9
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-04 08:25:50 -0500

    clean up and testing

--- Scripts/Event/DUA;PLANNING!~!~!~.js
+++ Scripts/Event/DUA;PLANNING!~!~!~.js
@@ -3,6 +3,9 @@ try {
 	if (publicUser && matches(capStatus, "Pending Applicant")) { // && matches(wfTask,'Review Distribution','Recordation')) {
         updateAppStatus("New Documents Uploaded", "Update by Document Upload from Cutomer");
     }
+	if (!publicUser && matches(capStatus, "Pending Applicant")) { // && matches(wfTask,'Review Distribution','Recordation')) {
+        updateAppStatus("New Documents Uploaded", "Update by Back Office Upload");
+    }
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
