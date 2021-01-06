6a08187c43efba631cb19b55c9fab142d8c8ffe1
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-06 07:01:38 -0500

    clean up per Melissa...

--- Scripts/Event/WTUA;BUILDING!PERMIT!COMMERCIAL!~.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!COMMERCIAL!~.js
@@ -32,6 +32,37 @@ try {
 				}
 			}
 		}
+		if (wfTask =='Inspections' && wfStatus == 'Amendment Submitted') {
+			var newAppTypeString = appTypeArray[0] + "/" + appTypeArray[1] + "/" + appTypeArray[2] + "/" + "Amendment";
+			if (appMatch("*/*/*/NA")) {
+				var newAppTypeString = "Building/Permit/Commercial/Amendment";
+				updateTask("Inspections", "Amendment Submitted", "Updated by Workflow Selection", "");
+			}
+			else if (!appMatch("*/*/*/NA")) {
+				var newAppTypeString = "Building/Permit/Commercial/AmendmentTrade";
+				updateTask("Inspections", "Amendment Submitted", "Updated by Workflow Selection", "");
+			}
+			var newCapName = capName;
+			var newCapIdString = getNextChildCapId(capId, newAppTypeString, "-");
+			var newCapRelation = "Child";
+			var srcCapId = capId;
+			var newCapId = createCap_TPS(newAppTypeString, newCapName, newCapIdString, newCapRelation, srcCapId);
+			if (newCapId) {
+				showMessage = true;
+				comment("<b>Created " + (newCapRelation ? newCapRelation + " " : "")
+					+ "Amendment: <b>" + newCapId.getCustomID() + "</b> " + newAppTypeString);
+				if (wfComment && wfComment != "") {
+					cWorkDesc = workDescGet(capId);
+					nWorkDesc = cWorkDesc + ", " + wfComment;
+					updateWorkDesc(nWorkDesc, newCapId);
+				}
+				// set the Permit Expiration Date to 180 days from Application Date.
+				var expDateField = "Permit Expiration Date";
+				var expDate = jsDateToASIDate(new Date(dateAdd(null, 180)));
+				editAppSpecific(expDateField, expDate, newCapId);
+			}
+		}
+		addFee("ADMIN", "CC-BLD-ADMIN", "FINAL", 1, "Y");
 	}
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
