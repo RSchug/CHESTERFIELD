2ac98f4ad436df1cac31a1b7863181f50abe8e34
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-06 21:10:33 -0500

    clean up from Nick G

--- Scripts/_INCLUDES/INCLUDES_CUSTOM.js
+++ Scripts/_INCLUDES/INCLUDES_CUSTOM.js
@@ -4888,19 +4888,16 @@ function getInspectorObj(inspectorId) {
 }
 
 //Used for Inspection Sequence tracking requirement
-function getinsptypecount(capid,insptype)
-{
-var count = 0;
-var insp = aa.inspection.getInspections(capid).getOutput();
-for (x in insp)
-{
+function getinsptypecount(capid, insptype) {
+    var count = 0;
+    var insp = aa.inspection.getInspections(capid).getOutput();
+    for (x in insp) {
 
-if (insp[x].getInspectionType() == insptype)
-{count++
-
-}
-}
-return count;
+        if (insp[x].getInspectionType() == insptype && insp[x].getInspectionStatus() != "Cancelled") {
+            count++
+        }
+    }
+    return count;
 }
 
 function getLastInspectioncomment(insp2Check)
