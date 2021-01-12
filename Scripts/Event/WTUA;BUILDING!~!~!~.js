50b250a8f27611b2561d3cc7530f6709789eba80
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-12 12:18:04 -0500

    clean up

--- Scripts/Event/WTUA;BUILDING!~!~!~.js
+++ Scripts/Event/WTUA;BUILDING!~!~!~.js
@@ -16,11 +16,13 @@ try {
 		loadCustomScript("WTUA_EXECUTE_DIGEPLAN_SCRIPTS_BUILD");
 	}
 //For PROFFER
-	if(appMatch("Building/Permit/Commercial/NA") && AInfo["Nature of Work"] == "New Construction" && wfTask == 'Review Distribution' && wfStatus == 'Routed' && parcelHasCondition_TPS("Budget","Applied")) {
-		activateTask("Budget and Management Review");
-	}
-	if((appMatch("Building/Permit/Residential/NA") || appMatch("Building/Permit/Residential/Multi-Family"))&& AInfo["Nature of Work"] == "New Construction of Single Family Dwelling" && wfTask == 'Review Distribution' && wfStatus == 'Routed' && parcelHasCondition_TPS("Budget","Applied")) {
-		activateTask("Budget and Management Review");
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed of Review') {
+		if(appMatch("Building/Permit/Commercial/NA") && AInfo["Nature of Work"] == "New Construction" && parcelHasCondition_TPS("Budget","Applied")) {
+			activateTask("Budget and Management Review");
+		}
+		if((appMatch("Building/Permit/Residential/NA") || appMatch("Building/Permit/Residential/Multi-Family")) && AInfo["Nature of Work"] == "New Construction of Single Family Dwelling" && parcelHasCondition_TPS("Budget","Applied")) {
+			activateTask("Budget and Management Review");
+		}
 	}
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
