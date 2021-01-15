8ed5ad2da87841a70dbc003e2d6a4049544c7427
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-15 16:01:32 -0500

    clean up

--- Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
@@ -6,6 +6,9 @@ if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo['
 	if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo['Request type'] != 'Communication Tower'){
 		addFee("SAOTHER","CC-PLANNING","FINAL",1,"N");
 	}
+	if (wfTask == 'Application Submittal' && wfStatus == 'Ready for Payment') {
+		invoiceAllFees(capId);
+	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
