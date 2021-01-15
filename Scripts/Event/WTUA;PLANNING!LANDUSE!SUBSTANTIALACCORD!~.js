4beef7b382f271a32b54eccd5b06cb0a45307fea
bushatos <66446858+bushatos@users.noreply.github.com>
2021-01-15 15:53:28 -0500

    clean up

--- Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
+++ Scripts/Event/WTUA;PLANNING!LANDUSE!SUBSTANTIALACCORD!~.js
@@ -1,9 +1,9 @@
 try {
 	//Fees for Substantial Accord
-if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo[Request type] == 'Communication Tower') {
+if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo['Request type'] == 'Communication Tower') {
 		addFee("SACTOWER","CC-PLANNING","FINAL",1,"N");
 	}
-	if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo[Request type] != 'Communication Tower'){
+	if (wfTask == 'Application Submittal' && wfStatus == 'Calculate Fees' && AInfo['Request type'] != 'Communication Tower'){
 		addFee("SAOTHER","CC-PLANNING","FINAL",1,"N");
 	}
 } catch (err) {
