01d295e6945b95a85fe3759f45be8b602c11511a
SYSTEM <SYSTEM@NONPROD-AA-1.us.accela.com>
2021-01-28 11:36:01 -0600

    commit by emse tools 2021-01-28 11:36:01

--- Scripts/Event/AIUA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
+++ Scripts/Event/AIUA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
@@ -1,6 +1,4 @@
-//Update Fees if additional information is updated//
 if (!publicUser) {
-updateFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",houseCount,"Y");
+	var NumberofUnits = AInfo["Number of Units"];
+	updateFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",NumberofUnits,"Y");
 }
-//updateFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y","Y");
-//aa.finance.reCalculateFees("STATELEVY","CONTRACTOR",1); 
