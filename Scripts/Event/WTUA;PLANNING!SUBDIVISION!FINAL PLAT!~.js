8b55e3ddbfae11fa0e94dcdf536b0f405a8876b2
bushatos <66446858+bushatos@users.noreply.github.com>
2021-02-04 08:45:21 -0500

    clean up

--- Scripts/Event/WTUA;PLANNING!SUBDIVISION!FINAL PLAT!~.js
+++ Scripts/Event/WTUA;PLANNING!SUBDIVISION!FINAL PLAT!~.js
@@ -18,17 +18,13 @@ try {
 	if (wfTask == 'Review Distribution' && matches(wfStatus,'Routed for Review','Manual Routing')) {		
 	//per the ELM Planning Due Dates Doc
 		var workflowTasks = aa.workflow.getTasks(capId).getOutput();
-		var taskAuditArray = ['Airport Review','Assessor Review','Building Inspection Review','Budget and Management Review','Community Enhancement Review','County Library Review','Chesterfield Historical Society Review','Health Department Review','CDOT Review','Economic Development Review','Environmental Engineering Review','Fire and Life Safety Review','GIS-EDM Utilities Review','GIS-IST Review','Parks and Recreation Review','Planning Review','Police Review','Real Property Review','Schools Research and Planning Review','County Attorney Review','Utilities Review','VDOT Review','Water Quality Review'];
-		for (var ind in taskAuditArray) {
-			var wfaTask = taskAuditArray[ind];
-			for (var i in workflowTasks) {
-				var wfbTask = workflowTasks[i];
-				if (wfbTask.getActiveFlag() == 'Y') {
-					if (capStatus == 'Submit Signed Plat') {
-						editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,5,true));
-					}
-					else { editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,10,true)); }
+		for (var i in workflowTasks) {
+			var wfbTask = workflowTasks[i];
+			if (wfbTask.getActiveFlag() == 'Y') {
+				if (capStatus == 'Submit Signed Plat') {
+					editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,5,true));
 				}
+				else { editTaskDueDate(wfbTask.getTaskDescription(),dateAdd(null,10,true)); }
 			}
 		}
 	}
