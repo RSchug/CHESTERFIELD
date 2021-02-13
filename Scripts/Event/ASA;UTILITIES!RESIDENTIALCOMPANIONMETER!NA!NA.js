8f0bc17ae66d02c916b697bcc92096fbad9dcaee
SYSTEM <SYSTEM@mtprod-AA-7.us.accela.com>
2021-02-13 12:27:41 -0600

    commit by emse tools 2021-02-13 12:27:41

--- Scripts/Event/ASA;UTILITIES!RESIDENTIALCOMPANIONMETER!NA!NA.js
+++ Scripts/Event/ASA;UTILITIES!RESIDENTIALCOMPANIONMETER!NA!NA.js
@@ -1,7 +1,7 @@
-var newTapNumber = Number(lookup("NEW_TAP_NUMBER","TAP_NUMBER"));
+var newTapNumber = Number(lookup("NEW_CONNECTION_SERVICE_NUMBER","SERVICE_NUMBER"));
 var count = 1;
-editAppSpecific("Service Number",lookup("NEW_TAP_NUMBER","TAP_NUMBER"));
-editLookup("NEW_TAP_NUMBER","TAP_NUMBER",zeroPad(newTapNumber+count,8));
+editAppSpecific("Service Number",lookup("NEW_CONNECTION_SERVICE_NUMBER","SERVICE_NUMBER"));
+editLookup("NEW_CONNECTION_SERVICE_NUMBER","SERVICE_NUMBER",zeroPad(newTapNumber+count,8));
 
 if (!publicUser) performCISLookup()
 //Add Fees//
