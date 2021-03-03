f92fb0f2f320dbed1f819c2f85c5bbe299c0e7ab
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-03 16:06:17 -0500

    update for emails...

--- Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!NA.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!RESIDENTIAL!NA.js
@@ -128,6 +128,37 @@ try {
 			}
 		}
 	}
+	if (wfTask == "Structural Review" && wfStatus == "Corrections Required"){
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_CONTACT NOTIFICATION_CORRECTION_BLD";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+		
+	}
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
--- Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
@@ -1,38 +1,6 @@
 try {
 	//Permit Issuance is Issued than updated Permit Expiration Date to 180 days from system date
 	if ((wfTask == "Permit Issuance" && wfStatus == "Issued") || !exists(capStatus, ["Cancelled","Pending Applicant"])) { 
-		//01-2021 moved the auto-email from the DigEplan scripts to here - this is not working...
-		//var ApprovedStatus = 'Issued'; var docGroupArrayModule = 'General';
-		//emailReviewCompleteNotification_BLD(ApprovedStatus, docGroupArrayModule);
-		
-		var emailSendFrom = '';
-		var emailSendTo = "";
-		var emailCC = "";
-		var emailTemplate = "WTUA_CONTACT NOTIFICATION_APPROVED_BLD";
-		var fileNames = [];
-		var emailParameters = aa.util.newHashtable();
-		getRecordParams4Notification(emailParameters);
-		getAPOParams4Notification(emailParameters);
-		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
-		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
-		//getACARecordParam4Notification(emailParameters,acaSite);
-		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
-		addParameter(emailParameters, "$$wfComment$$", wfComment);
-		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
-		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
-		var applicantEmail = "";
-		var contObj = {};
-		contObj = getContactArray(capId);
-		if (typeof(contObj) == "object") {
-			for (co in contObj) {
-				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
-					applicantEmail += contObj[co]["email"] + ";";
-			}
-			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
-		} else { logDebug("No contacts at all for " + capIDString); }
-		if (applicantEmail != "") {
-			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
-		} else { logDebug("No applicants for " + capIDString); }
 		
 	// Update Permit Expiration Date on record, and where appropriate parent and children
 		var expField = "Permit Expiration Date";
@@ -87,6 +55,40 @@ try {
 	if (wfStatus == 'Temporary Certificate Issued' && appMatch("Building/Permit/Elevator/Renewal")) {
 	editAppSpecific(tempcertexpdate,tempcertexpdatenew);
 	}
+	/*f (wfTask == "Permit Issuance" && wfStatus == "Issued") { 
+		//01-2021 moved the auto-email from the DigEplan scripts to here - this is not working...
+		//var ApprovedStatus = 'Issued'; var docGroupArrayModule = 'General';
+		//emailReviewCompleteNotification_BLD(ApprovedStatus, docGroupArrayModule);
+		
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_CONTACT NOTIFICATION_APPROVED_BLD";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}*/
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
